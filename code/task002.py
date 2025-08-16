6fa36e7cbdc2586ad76d5e7ccc021332039ff872
moto <mo_to1015@yahoo.co.jp>
2025-08-17 02:53:24 +0900

    数字とfor文をくっつけた

--- code/task002.py
+++ code/task002.py
@@ -3,4 +3,4 @@ def p(g):
  def f(i,j):
   if(0<=i<n)*(0<=j<n)and v[i][j]*(g[i][j]<1):v[i][j]=0;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
  for i in r:f(i,0);f(i,n-1);f(0,i);f(n-1,i)
- return[[4 if(g[i][j]<1)*v[i][j] else g[i][j]for j in r]for i in r]
+ return[[4if(g[i][j]<1)*v[i][j]else g[i][j]for j in r]for i in r]
