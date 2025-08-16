dd95bc1db089c0797ae2c02c360b70c4a359498d
KazApps <kazuki_shogi@outlook.jp>
2025-08-16 21:38:35 +0900

    verify.py、show.pyを調整

--- input/google-code-golf-2025/code_golf_utils/show.py
+++ input/google-code-golf-2025/code_golf_utils/show.py
@@ -1,10 +1,14 @@
 import code_golf_utils
 import matplotlib.pyplot as plt
+import sys
 
-task_num = 4
+task_num = 5
 task_name = "arc-gen"  # train, test, arc-gen
 s = 60
 num = 5
+
+if len(sys.argv) > 1:
+    task_num = int(sys.argv[1])
 
 examples = code_golf_utils.load_examples(task_num=task_num)[task_name][s:s+num]
 code_golf_utils.show_examples(examples=examples)
--- input/google-code-golf-2025/code_golf_utils/verify.py
+++ input/google-code-golf-2025/code_golf_utils/verify.py
@@ -1,7 +1,12 @@
 import code_golf_utils
+import sys
 
-task_num = 4
-task_path = "./code/task4.py"
+task_num = 5
+
+if len(sys.argv) > 1:
+    task_num = int(sys.argv[1])
+
+task_path = f"./code/task{str(task_num).zfill(3)}.py"
 examples = code_golf_utils.load_examples(task_num=task_num)
 code_golf_utils.verify_program(
     task_num=task_num, examples=examples, task_path=task_path
