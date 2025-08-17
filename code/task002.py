4c4394f8e981c7d6129a44fb50bfedb119979626
KazApps <kazuki_shogi@outlook.jp>
2025-08-17 15:39:01 +0900

    文字コードを変更

--- code/task002.py
+++ code/task002.py
@@ -1,6 +1,6 @@
 def p(g):
  n=len(g);r=range(n);v=[[1]*n for _ in r]
  def f(i,j):
-  if(0<=i<n)*(0<=j<n)and v[i][j]*(g[i][j]<1):v[i][j]=0;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
+  if(0<=i<n)*(0<=j<n)and(g[i][j]<1)*v[i][j]:v[i][j]=0;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
  for i in r:f(i,0);f(i,n-1);f(0,i);f(n-1,i)
- return[[4if(g[i][j]<1)*v[i][j]else g[i][j]for j in r]for i in r]
+ return[[(g[i][j],4)[(g[i][j]<1)*v[i][j]]for j in r]for i in r]
