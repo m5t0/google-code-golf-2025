02e695958c9102df436a95e7075b920828be24b3
moto <mo_to1015@yahoo.co.jp>
2025-08-18 14:49:53 +0900

    task008のコードを短くした

---
+++ memo/memo008.py
@@ -0,0 +1,2 @@
+# 変数をたくさん使って実装
+def p(g,e=enumerate):h=lambda n:[(i,j)for i,r in e(g)for j,v in e(r)if v==n];a,c=h(2),h(8);u=lambda l:[*map(min,zip(*l)),*map(max,zip(*l))];t,l,b,r,T,L,B,R=u(a)+u(c);return[[2*((i-(b<T)*(T-b-1)-(B<t)*(B-t+1),j-(r<L)*(L-r-1)-(R<l)*(R-l+1))in a)+8*((i,j)in c)for j,w in e(v)]for i,v in e(g)]
