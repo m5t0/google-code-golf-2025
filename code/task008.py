1c3a13f905394716a83e56ecff84c47089bc9c9a
moto <mo_to1015@yahoo.co.jp>
2025-08-18 23:15:22 +0900

    task008.pyの文字数を削減

--- code/task008.py
+++ code/task008.py
@@ -1,1 +1,5 @@
-def p(g,e=enumerate):h=lambda n:[(i,j)for i,r in e(g)for j,v in e(r)if v==n];a,c=h(2),h(8);u,z=lambda l:[*map(min,zip(*l)),*map(max,zip(*l))],lambda t,b,T,B:(b<T)*(T-b-1)+(B<t)*(B-t+1);y=u(a)+u(c);return[[2*((i-z(*y[::2]),j-z(*y[1::2]))in a)+8*((i,j)in c)for j,w in e(v)]for i,v in e(g)]
+def p(g,e=enumerate):
+ h=lambda n:[(i,j)for i,r in e(g)for j,v in e(r)if v==n];a,c=h(2),h(8);u,z=lambda l:[*map(min,*l),*map(max,*l)],lambda t,b,T,B:(b<T)*(T+~b)+(B<t)*(B-t+1);y=u(a)+u(c)
+ for i,j in a:g[i][j]=0
+ for i,j in a:g[i+z(*y[::2])][j+z(*y[1::2])]=2
+ return g
