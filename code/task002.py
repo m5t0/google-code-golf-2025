ba80e6f68ce8332c1f79790d05eba638a604e302
KazApps <kazuki_shogi@outlook.jp>
2025-08-18 16:39:41 +0900

    task002.pyの文字数を削減

--- code/task002.py
+++ code/task002.py
@@ -1,5 +1,5 @@
 def p(g):
- n=len(g);g=[[4-(b>0)for b in a]for a in g]
+ n=len(g);g=[[4-b%2for b in a]for a in g]
  def f(i,j):
   if(0<=i<n)*(0<=j<n)and g[i][j]>3:g[i][j]=0;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
  for i in range(n):f(i,0);f(i,n-1);f(0,i);f(n-1,i)
