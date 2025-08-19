8871b883e682012f09777e22c9a30bc6d4658df8
KazApps <kazuki_shogi@outlook.jp>
2025-08-20 01:08:10 +0900

    task012.pyの文字数を削減

--- code/task012.py
+++ code/task012.py
@@ -1,7 +1,5 @@
-r=range;s=r(2,10)
-def p(g):
+def p(g,s=range(-2,10)):
  for i in s:
   for j in s:
-   if c:=g[i][j]*((n:=g[i+1][j])==g[i-1][j]==g[i][j+1]>0):
-    for a in r(-2,3):g[i-a][j]=g[i][j-a]=n;g[i+a][j+a]=g[i+a][j-a]=c
+   for a in((n:=g[i+1][j])==g[i-1][j]==g[i][j+1]>0)*[*s[:5]]:c=g[i][j];g[i-a][j]=g[i][j-a]=n;g[i+a][j+a]=g[i+a][j-a]=c
  return g
