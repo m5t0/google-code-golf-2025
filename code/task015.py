a36e915d030cfe66161c41b6224ef6dc24ebafdf
KazApps <kazuki_shogi@outlook.jp>
2025-08-20 15:20:26 +0900

    task015.pyを短縮

--- code/task015.py
+++ code/task015.py
@@ -1,5 +1,6 @@
-def p(g,r=range):
- for i in r(81):
-  for j in r(9):
-   if(a:=j//3-1)|(b:=j%3-1)and(c:=7-3*(a*b!=0))==10-3*g[i//9][i%9]and g[n:=i//9+a][m:=i%9+b]<1:g[n][m]=c
+def p(g,r=range(9)):
+ for i in r:
+  for j in r:
+   if 0<(c:=g[i][j])<3:
+    g[i+c-1][j+1]=g[i+1][j-c+1]=g[i-1][j+c-1]=g[i-c+1][j-1]=6//c+1
  return g
