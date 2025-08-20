88b0186992530391d0afb81e861ae9aaa111762d
KazApps <kazuki_shogi@outlook.jp>
2025-08-20 12:05:31 +0900

    task008.pyの文字数を削減

--- code/task008.py
+++ code/task008.py
@@ -1,5 +1,5 @@
 def p(g,e=enumerate):
- h=lambda n:[(i,j)for i,r in e(g)for j,v in e(r)if v==n];a,c=h(2),h(8);u,z=lambda l:[*map(min,*l),*map(max,*l)],lambda t,b,T,B:(b<T)*(T+~b)+(B<t)*(B-t+1);y=u(a)+u(c)
+ h=lambda n:[(i,j)for i,r in e(g)for j,v in e(r)if v&n];a,u,z=h(2),lambda l:[*map(min,*l),*map(max,*l)],lambda t,b,T,B:(b<T)*(T+~b)+(B<t)*(B-t+1);y=u(a)+u(h(8))
  for i,j in a:g[i][j]=0
  for i,j in a:g[i+z(*y[::2])][j+z(*y[1::2])]=2
  return g
