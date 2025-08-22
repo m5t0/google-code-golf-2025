65803452df5fe9ed83b76335859295d29d4b643d
moto <mo_to1015@yahoo.co.jp>
2025-08-23 02:25:16 +0900

    task020.pyの短縮

--- code/task020.py
+++ code/task020.py
@@ -1,9 +1,8 @@
 def p(g,r=range):
  for s in r(36):
-  v,c,p,q=[0]*9,0,s//6,s%6
+  v,c,p,q=[0]*9,0,s//6+2,s%6+2
   for k in r(25):
-   if((m:=k//5-2)+(n:=k%5-2))%2<1:c+=(g[i:=2+p+m][j:=2+q+n]>0);v[x]=max(v[x:=m*m+n*n],g[i][j]);
-  if c>9:break
- for k in r(25):
-  g[2+p+m][2+q+n]=v[(m:=k//5-2)**2+(n:=k%5-2)**2]*((m+n)%2<1)
- return g
+   if((m:=k//5-2)+(n:=k%5-2))%2<1:c+=(g[i:=p+m][j:=q+n]>0);v[x]=g[i][j]or v[x:=m*m+n*n]
+  if c>9:
+   for k in r(25):g[p+m][q+n]=v[(m:=k//5-2)**2+(n:=k%5-2)**2]*((m+n)%2<1)
+   return g
