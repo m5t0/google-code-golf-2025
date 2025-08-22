3bad7e6a40b8fcb8a9bcf74014b992297e2fbc7a
sktkmozt <kazu22kun@icolud.com>
2025-08-22 18:58:38 +0900

    100bytes

--- code/task399.py
+++ code/task399.py
@@ -1,2 +1,1 @@
-def p(g,r=range(3)):
- return[[(~(k:=3*i+j)%2&(sum(x!=0for a in g for x in a)//2>k))for j in r]for i in r]
+p=lambda g,r=range(3):[[~(k:=3*i+j)%2&(sum(x!=0 for a in g for x in a)//2>k) for j in r] for i in r]
