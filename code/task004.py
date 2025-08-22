253285ae2dfe17abc64b5a6ea06c4d0d66cc7ae6
KazApps <kazuki_shogi@outlook.jp>
2025-08-22 23:48:57 +0900

    task004.pyを短縮

--- code/task004.py
+++ code/task004.py
@@ -3,5 +3,5 @@ def p(g):
  for a in g[::-1]:
   for i in range(len(a))[::-1]:
    v[x]=max(v[x:=a[i]],i)
-   if(0<i<=v[y:=a[i-1]])*y:a[i],a[i-1]=y,x
+   if v[y:=a[i-1]]>=i>0<y:a[i],a[i-1]=y,x
  return g
