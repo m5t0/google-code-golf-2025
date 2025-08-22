59105f4cce00c9e556c2856298b75d8006254337
KazApps <kazuki_shogi@outlook.jp>
2025-08-22 21:11:18 +0900

    sktkmoztブランチのtask015.pyを短縮してマージ

--- code/task015.py
+++ code/task015.py
@@ -1,6 +1,4 @@
-def p(g,r=range(9)):
- for i in r:
-  for j in r:
-   if 0<(c:=g[i][j])<3:
-    g[i+c-1][j+1]=g[i+1][j-c+1]=g[i-1][j+c-1]=g[i-c+1][j-1]=6//c+1
+def p(g):
+ for k in range(81):
+  if 0<(c:=g[i:=k//9][j:=k%9])<3:g[i+c-1][j+1]=g[i+1][j-c+1]=g[i-1][j+c-1]=g[i-c+1][j-1]=6//c+1
  return g
