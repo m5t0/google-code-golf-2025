32720c80a6d7b87a8f5f4f301f331a2fdfc18b38
sktkmozt <kazu22kun@icolud.com>
2025-08-22 18:57:02 +0900

    118bytes

--- code/task400.py
+++ code/task400.py
@@ -1,4 +1,1 @@
-def p(g,r=range(5)):
- for i in range(576):
-  x,y=i//24,i%24
-  if g[x][y]==1:return[[g[23-x-a][23-y-b]for b in r]for a in r]
+p=lambda g,r=range(5):next([[g[23-x-a][23-y-b]for b in r]for a in r]for i in range(576)if g[(x:=i//24)][(y:=i%24)]==1)
