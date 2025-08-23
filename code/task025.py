c0a647186bdc148883eab344d8861240a60a9db8
moto <mo_to1015@yahoo.co.jp>
2025-08-23 20:05:03 +0900

    task025.pyのコードを短縮

--- code/task025.py
+++ code/task025.py
@@ -1,8 +1,8 @@
-def p(g,r=range):
- g=[[*zip(*g)],g][v:=any(len({*[*zip(*g)][j]})<2for j in r(len(g[0])))];m,n=len(g),len(g[0]);o=[n*[0]for _ in r(m)]
- for s in r(m*n):
-  if(c:=g[i:=s//n][j:=s%n])*(len({*([*zip(*g)][j])})<2):
-   o[i][j]=c
-   if j<n-1:o[i][j+1]=c*(c in g[i][j+1:])
-   if j:o[i][j-1]=c*(c in g[i][:j])
- return [[[*[*zip(*o)][j]]for j in r(n)],o][v]
+def p(g):
+ z=[*zip(*g)];v=any(len({*c})<2for c in z);g=[z,g][v];n=len(g[0]);m=len(g);o=[n*[0]for _ in[0]*m]
+ for s in range(m*n):
+  if(c:=g[i:=s//n][j:=s%n])and len({*[*zip(*g)][j]})<2:
+   a=o[i];a[j]=c
+   if j<n-1:a[j+1]=c*(c in g[i][j+1:])
+   if j:a[j-1]=c*(c in g[i][:j])
+ return([*map(list,zip(*o))],o)[v]
