ce9189c8a16fd5fb367fc45c03a3b77ecde63144
KazApps <kazuki_shogi@outlook.jp>
2025-08-23 17:20:19 +0900

    task025.pyを短縮

--- code/task025.py
+++ code/task025.py
@@ -1,12 +1,8 @@
-def p(g):
- r,m,n=range,len(g),len(g[0]);o=[n*[0]for _ in r(m)]
+def p(g,r=range):
+ g=[t:=[*zip(*g)],g][v:=any(len({*t[j]})<2for j in r(len(g[0])))];m,n=len(g),len(g[0]);o=[n*[0]for _ in r(m)]
  for s in r(m*n):
-  if(c:=g[i:=s//n][j:=s%n])*(len({*(h:=[*zip(*g)][j])})<2):
+  if(c:=g[i:=s//n][j:=s%n])*(len({*([*zip(*g)][j])})<2):
    o[i][j]=c
    if j<n-1:o[i][j+1]=c*(c in g[i][j+1:])
    if j:o[i][j-1]=c*(c in g[i][:j])
-  if(len({*g[i]})<2)*c:
-   o[i][j]=c
-   if i<m-1:o[i+1][j]=c*(c in h[i+1:])
-   if i:o[i-1][j]=c*(c in h[:i])
- return o
+ return[[[*[*zip(*o)][j]]for j in r(n)],o][v]
