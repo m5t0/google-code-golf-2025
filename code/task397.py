8ed71905a24acf43ba5dfacc965497ec3911b262
KazApps <kazuki_shogi@outlook.jp>
2025-08-23 12:15:13 +0900

    task397.pyを短縮

--- code/task397.py
+++ code/task397.py
@@ -1,4 +1,4 @@
 def p(g,r=range):
  for I in r(81)[::-1]:
-  for k in((x:=g[i:=I//9][j:=I%9])*(y:=g[i+1][j])*(z:=g[i][j+1])>0)*[*r(len({x,y,z,g[i+1][j+1]}))]:g[i+2+k][j]=g[i+2+k][j+1]=3
+  for k in((x:=g[i:=I//9][j:=I%9])*(y:=g[i+1][j])*(z:=g[i][s:=j+1]))*[*r(len({x,y,z,g[i+1][s]}))]:g[t:=i+2+k][j]=g[t][s]=3
  return g
