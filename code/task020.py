ec98b72a580d80046be743307dd24ea0033decbb
moto <mo_to1015@yahoo.co.jp>
2025-08-23 11:01:19 +0900

    task020.pyの修正

--- code/task020.py
+++ code/task020.py
@@ -2,7 +2,7 @@ def p(g,r=range):
  for s in r(36):
   v,c,p,q=[0]*9,0,s//6+2,s%6+2
   for k in r(25):
-   if((m:=k//5-2)+(n:=k%5-2))%2<1:c+=(g[i:=p+m][j:=q+n]>0);v[x]=g[i][j]or v[x:=m*m+n*n]
+   if((m:=k//5-2)+(n:=k%5-2))%2<1:c+=(g[i:=p+m][j:=q+n]>0);v[x]=v[x:=m*m+n*n]or g[i][j]
   if c>9:
    for k in r(25):g[p+m][q+n]=v[(m:=k//5-2)**2+(n:=k%5-2)**2]*((m+n)%2<1)
    return g
