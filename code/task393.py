fc3f63a97d5efa7af3440888d16390b1e790d00f
sktkmozt <kazu22kun@icolud.com>
2025-08-24 03:03:09 +0900

    task393-120bytes

--- code/task393.py
+++ code/task393.py
@@ -2,5 +2,5 @@ def p(g):
  d={}
  for r in g:
   for x in r:
-   if x:d[x]=d.get(x,0)+1
- return [[x]for x in sorted(d,key=d.get,reverse=1)[:3]]
+   d[x]=d.get(x,0)+1
+ return [[x]for x in sorted(d,key=d.get,reverse=1)[1:4]]
