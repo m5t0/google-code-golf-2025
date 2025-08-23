6ea4282828f8292eb9663854024b433edad6db84
KazApps <kazuki_shogi@outlook.jp>
2025-08-23 23:01:12 +0900

    motoブランチのtask025.pyを短縮してマージ

--- code/task025.py
+++ code/task025.py
@@ -1,8 +1,7 @@
-def p(g,r=range):
- g=[t:=[*zip(*g)],g][v:=any(len({*t[j]})<2for j in r(len(g[0])))];m,n=len(g),len(g[0]);o=[n*[0]for _ in r(m)]
- for s in r(m*n):
-  if(c:=g[i:=s//n][j:=s%n])*(len({*([*zip(*g)][j])})<2):
-   o[i][j]=c
-   if j<n-1:o[i][j+1]=c*(c in g[i][j+1:])
-   if j:o[i][j-1]=c*(c in g[i][:j])
- return[[[*[*zip(*o)][j]]for j in r(n)],o][v]
+def p(g):
+ l=len;g=[z:=[*zip(*g)],g][v:=any(l({*c})<2for c in z)];n=l(g[0]);o=[n*[0]for _ in[0]*l(g)]
+ for s in range(l(g)*n):
+  if(c:=(b:=g[i:=s//n])[j:=s%n])*(l({*[*zip(*g)][j]})<2):
+   a=o[i];a[j]=c;a[j-1]=c*(c in b[:j])
+   if j<n-1:a[j+1]=c*(c in b[j+1:])
+ return([*map(list,zip(*o))],o)[v]
