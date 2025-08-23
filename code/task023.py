7a5777d3aeb4d1237359c1823a9eeb8b7cbf79e2
KazApps <kazuki_shogi@outlook.jp>
2025-08-23 15:33:35 +0900

    task023.pyを短縮

--- code/task023.py
+++ code/task023.py
@@ -1,15 +1,15 @@
 ﻿def p(g,e=exec):
- n=len(g);m=len(g[0])
+ n=len(g);m=len(g[0]);s="g[i][j]=g[v][w]=g[v"
  for I in range(n*m):
   if g[i:=I//m][j:=I%m]&5:
-   if i<n-1and j<m-1and g[t:=i+1][j]==g[i][u:=j+1]==g[t][u]==5:
-    s="g[i][j]=g[t][j]=g[i][u]=g[t][u]=";e(s+"8")
+   if i<n-1and j<m-1and g[v:=i+1][j]==g[i][w:=j+1]==g[v][w]==5:
+    t=s+"][j]=g[i][w]=";e(t+"8")
     if p(g):return g
-    e(s+"5")
+    e(t+"5")
    for x,y in(0,1),(1,0):
-    if[i,j][y]<[n,m][y]-2and g[t:=i+x][u:=j+y]==g[t+x][u+y]==5:
-     s="g[i][j]=g[t][u]=g[t+x][u+y]=";e(s+"2")
+    if[i,j][y]<[n,m][y]-2and g[v:=i+x][w:=j+y]==g[v+x][w+y]==5:
+     t=s+"+x][w+y]=";e(t+"2")
      if p(g):return g
-     e(s+"5")
+     e(t+"5")
    return 0
  return 1
