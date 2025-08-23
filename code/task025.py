933b1cce83e5895b70d454d35b378520f31664b6
moto <mo_to1015@yahoo.co.jp>
2025-08-23 15:36:21 +0900

    task025.pyの修正

--- code/task025.py
+++ code/task025.py
@@ -1,13 +1,13 @@
 def p(g):
- r,m,n,f=range,len(g),len(g[0]),lambda c,l:c*any(x==c for x in l);o=[[0]*n for i in r(m)]
+ r,m,n=range,len(g),len(g[0]);o=[[0]*n for _ in r(m)]
  for s in r(m*n):
-   if(c:=g[i:=s//n][j:=s%n]):
-    if len({*(h:=[*zip(*g)][j])})<2:
-      o[i][j]=c
-      if j<n-1:o[i][j+1]=f(c,g[i][j+1:])
-      if j>0:o[i][j-1]=f(c,g[i][:j])
-    elif len({*g[i]})<2:
-      o[i][j]=c
-      if i<m-1:o[i+1][j]=f(c,h[i+1:])
-      if i>0:o[i-1][j]=f(c,h[:i])
+  if c:=g[i:=s//n][j:=s%n]:
+   if len({*(h:=[*zip(*g)][j])})<2:
+    o[i][j]=c
+    if j<n-1:o[i][j+1]=c*(c in g[i][j+1:])
+    if j>0:o[i][j-1]=c*(c in g[i][:j])
+   elif len({*g[i]})<2:
+    o[i][j]=c
+    if i<m-1:o[i+1][j]=c*(c in h[i+1:])
+    if i>0:o[i-1][j]=c*(c in h[:i])
  return o
