31338197eb1a244002494632ee00f7cd9f9aa022
moto <mo_to1015@yahoo.co.jp>
2025-08-24 10:05:36 +0900

    task023.pyを短縮したが圧縮した後の長さは変わらず

--- code/task023.py
+++ code/task023.py
@@ -2,7 +2,7 @@ def p(g):
  for i,r in enumerate(g):
   if 5in r:j=r.index(5);break
  else:return 1
- if 5in r[j+1:j+2]and(h:=g[i+1])[j:j+2]==[5,5]:
+ if r[j+1:j+2]==[5]and(h:=g[i+1])[j:j+2]==[5,5]:
   r[j:j+2]=h[j:j+2]=8,8
   if p(g):return g
   r[j:j+2]=h[j:j+2]=5,5
@@ -10,8 +10,7 @@ def p(g):
   r[j:j+3]=[2]*3
   if p(g):return g
   r[j:j+3]=[5]*3
- if [r[j]for r in g[i+1:i+3]]==[5,5]:
+ if[*zip(*g)][j][i+1:i+3]==(5,5):
   r[j]=g[i+1][j]=g[i+2][j]=2
   if p(g):return g
   r[j]=g[i+1][j]=g[i+2][j]=5
- return 0
