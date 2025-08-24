4bce7c8d786b6bfcf4b28c7676fcd29b54ea103a
KazApps <kazuki_shogi@outlook.jp>
2025-08-24 18:06:23 +0900

    sktkmoztブランチのtask396.pyを短縮してマージ

---
+++ code/task396.py
@@ -0,0 +1,9 @@
+def p(g,r=range):
+ v=sum(g,[]);s=min(v,key=v.count);c=w=l=e=0;L=len(g);R=len(g[0])
+ for I in r(L*R):
+  if n:=g[i:=I//R][j:=I%R]:
+   o=p=0
+   while o+i<L and g[i+o][j]==n:o+=1
+   while j+p<R and g[i][j+p]==n:p+=1
+   if o*p>l*e:c,w,l,e=i,j,o,p
+ return[[s*(g[c+i][w+j]>0)for j in r(e)]for i in r(l)]
