cacfe2351bc5ff7912cc0846e3417ccb3f4d3e1a
KazApps <kazuki_shogi@outlook.jp>
2025-08-24 17:00:25 +0900

    task391.pyとtask393.pyを短縮

--- code/task391.py
+++ code/task391.py
@@ -1,1 +1,2 @@
-from collections import*;p=lambda g:[[x]for x,_ in Counter(sum(g,[])).most_common()[2:5]]
+from collections import*
+p=lambda g:[[x]for x,_ in Counter(sum(g,[])).most_common()[2:]]
--- code/task393.py
+++ code/task393.py
@@ -1,1 +1,2 @@
-from collections import*;p=lambda g:[[x]for x,_ in Counter(sum(g,[])).most_common()[1:4]]
+from collections import*
+p=lambda g:[[x]for x,_ in Counter(sum(g,[])).most_common()[1:]]
