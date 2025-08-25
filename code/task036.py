ea28cfeb62a8ff46c3a68bdd369b6212e8ec056b
moto <mo_to1015@yahoo.co.jp>
2025-08-25 21:01:15 +0900

    task036.pyの短縮

--- code/task036.py
+++ code/task036.py
@@ -1,4 +1,1 @@
-def p(g):
- for c in range(1,10):
-  h=[[q*(q==c)for q in p]for p in g]
-  if(x:=[[v[j]for j in range(30)if any([*zip(*h)][j])]for v in h if any(v)])and sum(map(sum,x))//c>len(x)*len(x[0])//2:return x
+p=lambda g:[x for c in range(1,10)if(x:=[[v[j]*(v[j]==c)for j in range(30)if c in[*zip(*g)][j]]for v in g if c in v])and sum(map(sum,x))//c>len(x)*len(x[0])//2][0]
