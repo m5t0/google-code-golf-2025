bf606fe6bc0323336e4a68157c455406d4431d1c
sktkmozt <kazu22kun@icolud.com>
2025-08-26 07:20:07 +0900

    task390-362bytes

--- code/task390.py
+++ code/task390.py
@@ -2,9 +2,9 @@ def p(g,r=range):
  for i in r(12):
   if sum(x==2for x in g[i])>4:
    for j in r(2,4):
-    for k in r(12):g[i-j if i<7 else i+j][k],g[i+j if i<7 else i-j][k]=g[i+j if i<7 else i-j][k],0
+    for k in r(12):g[i-j if i<7else i+j][k],g[i+j if i<7else i-j][k]=g[i+j if i<7else i-j][k],0
  for j in r(12):
   if sum(g[i][j]==2for i in r(12))>4:
    for i in r(2,4):
-    for k in r(12):g[k][j-i if j<7 else j+i],g[k][j+i if j<7 else j-i]=g[k][j+i if j<7 else j-i],0
+    for k in r(12):g[k][j-i if j<7else j+i],g[k][j+i if j<7else j-i]=g[k][j+i if j<7else j-i],0
  return g
