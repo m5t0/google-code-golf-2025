5e72dcf2cd5b498444ca1f703c6d8ab374b7daeb
KazApps <kazuki_shogi@outlook.jp>
2025-08-26 21:25:03 +0900

    task387.pyを短縮

--- code/task387.py
+++ code/task387.py
@@ -1,11 +1,9 @@
 def p(g,r=range):
  u=0;n=len(g)
  for i in r(n):
-  v=[j for j,x in enumerate(g[i])if x%5]
-  if len(v)==2:
+  if len(v:=[j for j,x in enumerate(g[i])if x%5])==2:
    a,b=v
-   for k in r(9):
-    if k-4:g[t:=i+k//3-1][a+k%3-1]=g[i][b];g[t][b+k%3-1]=g[i][a]
+   for k in 0,1,2,3,5,6,7,8:g[t:=i+k//3-1][a+k%3-1]=g[i][b];g[t][b+k%3-1]=g[i][a]
    for c in r(1,b-a):
     if~min(c,b-a-c)&1:g[i][a+c]=5
    if u<1:
