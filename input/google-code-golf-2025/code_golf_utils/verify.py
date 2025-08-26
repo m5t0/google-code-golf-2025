19eeb7140300c24dd6073622e6bbd520be1ee7a9
KazApps <kazuki_shogi@outlook.jp>
2025-08-27 02:23:14 +0900

    verify.pyにtaskを複数指定できるように変更

--- input/google-code-golf-2025/code_golf_utils/verify.py
+++ input/google-code-golf-2025/code_golf_utils/verify.py
@@ -3,15 +3,14 @@ import sys
 
 task_num = 2
 
-if len(sys.argv) > 1:
-    task_num = int(sys.argv[1])
+if len(sys.argv) == 1:
+    sys.argv.append("2")
 
-task_path = f"./code/task{str(task_num).zfill(3)}.py"
+for i in range(1, len(sys.argv)):
+    task_num = int(sys.argv[i])
+    task_path = f"./code/task{str(task_num).zfill(3)}.py"
 
-if len(sys.argv) > 2:
-    task_path = sys.argv[2]
-
-examples = code_golf_utils.load_examples(task_num=task_num)
-code_golf_utils.verify_program(
-    task_num=task_num, examples=examples, task_path=task_path
-)
+    examples = code_golf_utils.load_examples(task_num=task_num)
+    code_golf_utils.verify_program(
+        task_num=task_num, examples=examples, task_path=task_path
+    )
