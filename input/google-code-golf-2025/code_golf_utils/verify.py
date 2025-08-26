4e13c522bd17e6521ee39f1fa74418ddce78c433
KazApps <kazuki_shogi@outlook.jp>
2025-08-27 03:51:06 +0900

    対象ディレクトリを指定できるようにした

--- input/google-code-golf-2025/code_golf_utils/verify.py
+++ input/google-code-golf-2025/code_golf_utils/verify.py
@@ -2,13 +2,18 @@ import code_golf_utils
 import sys
 
 task_num = 2
+task_dir = "./code/"
 
 if len(sys.argv) == 1:
-    sys.argv.append("2")
+    sys.argv.append(str(task_num))
+
+if not sys.argv[-1].isdigit():
+    task_dir = sys.argv[-1]
+    sys.argv.pop()
 
 for i in range(1, len(sys.argv)):
     task_num = int(sys.argv[i])
-    task_path = f"./code/task{str(task_num).zfill(3)}.py"
+    task_path = f"{task_dir}/task{str(task_num).zfill(3)}.py"
 
     examples = code_golf_utils.load_examples(task_num=task_num)
     code_golf_utils.verify_program(
