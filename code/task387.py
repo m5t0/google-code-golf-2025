43fe53578cca572494d84560cb47e4e507fae5b5
sktkmozt <kazu22kun@icolud.com>
2025-08-26 22:27:40 +0900

    task387-404bytes

--- code/task387.py
+++ code/task387.py
@@ -1,16 +1,7 @@
 def p(g,r=range):
- u=0;s=len(g)
- for i in r(s):
-  v=[j for j in r(len(g[0]))if g[i][j]%5]
-  if 1<len(v)<3:
-   for k in-1,0,1:
-    for l in-1,0,1:
-     if k|l:g[i+k][v[0]+l]=g[i][v[1]];g[i+k][v[1]+l]=g[i][v[0]]
-   for c in r(1,v[1]-v[0]):
-    if~min(c,v[1]-v[0]-c)&1:g[i][v[0]+c]=5
-   w=[x for x in r(s)if g[x][v[0]]%5]
-   if not u:
-    u=1
-    for c in r(1,w[3]-w[1]):
-     if~min(c,w[3]-w[1]-c)&1:g[w[1]+c][v[0]]=g[w[1]+c][v[1]]=5
+ a=[[i,j]for i in r(len(g))for j in r(len(g[0]))if g[i][j]>0];X,Y=zip(*a);m,k,l,o=min(X),max(X),min(Y),max(Y)
+ for i in r(len(g)):
+  for j in r(len(g[0])):
+   (x,y),d=min(((p,max(abs(i-p[0]),abs(j-p[1])))for p in a),key=lambda t:t[1])
+   g[i][j]=[[g[i][j],[v for v in g[x] if 0<v!=g[x][y]][0]][0<d<2],5][(~min(j-l,o-j)&1)*(i in X)*(l<j<o)|((~min(i-m,k-i)&1)*(j in Y)*(m<i<k))]
  return g
