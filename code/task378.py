d3ee4b2e782709ec42c48690533386c2f6226f36
KazApps <kazuki_shogi@outlook.jp>
2025-08-27 03:49:09 +0900

    task378.pyを短縮

--- code/task378.py
+++ code/task378.py
@@ -2,12 +2,12 @@ def p(g,r=range):
  d=0,1,0,-1;h=[*map(list,g)]
  for i in r(R:=len(g)):
   for j in r(R):
-   if g[i][j]<1:continue
+   if g[i][j]==0:continue
    for k in r(4):
-    x=(a:=d[k])+(b:=d[(k+1)%4]);y=b+(c:=d[(k+2)%4]);e=v=1
-    if R>i+a>-1<i+b<R>j+b>-1<j+c<R and g[i+a][j+b]<1>g[i+b][j+c]:
-     while(s:=j+e*y)>-1<(t:=i+e*x)<R>s:
-      if v>1:h[t][s]=h[i][j]
+    e=v=1
+    if R>i+d[k]>-1<i+d[(k+1)%4]<R>j+d[(k+1)%4]>-1<j+d[(k+2)%4]<R and g[i+d[k]][j+d[(k+1)%4]]<1>g[i+d[(k+1)%4]][j+d[(k+2)%4]]:
+     while(s:=j+e*(d[(k+1)%4]+d[(k+2)%4]))>-1<(t:=i+e*(d[k]+d[(k+1)%4]))<R>s:
+      if v>=2:h[t][s]=h[i][j]
       if g[t][s]:v+=1
       e+=1
  return h
