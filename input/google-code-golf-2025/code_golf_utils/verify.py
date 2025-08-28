802c6ec4f2e39b7da5b31c35f0c54d43be6ff22a
KazApps <kazuki_shogi@outlook.jp>
2025-08-28 15:15:05 +0900

    verify.pyにrangeを指定できるようにした

--- input/google-code-golf-2025/code_golf_utils/verify.py
+++ input/google-code-golf-2025/code_golf_utils/verify.py
@@ -1,4 +1,5 @@
 import code_golf_utils
+import os
 import sys
 
 task_num = 2
@@ -7,15 +8,26 @@ task_dir = "./code/"
 if len(sys.argv) == 1:
     sys.argv.append(str(task_num))
 
-if not sys.argv[-1].isdigit():
+if ".." not in sys.argv[-1] and not sys.argv[-1].isdigit():
     task_dir = sys.argv[-1]
     sys.argv.pop()
+
+for i in range(1, len(sys.argv))[::-1]:
+    if ".." in sys.argv[i]:
+        start, end = sys.argv[i].split("..")
+        sys.argv.pop(i)
+
+        for j in range(int(start), int(end) + 1):
+            sys.argv.append(str(j))
+
+print(sys.argv)
 
 for i in range(1, len(sys.argv)):
     task_num = int(sys.argv[i])
     task_path = f"{task_dir}/task{str(task_num).zfill(3)}.py"
 
-    examples = code_golf_utils.load_examples(task_num=task_num)
-    code_golf_utils.verify_program(
-        task_num=task_num, examples=examples, task_path=task_path
-    )
+    if os.path.isfile(task_path):
+        examples = code_golf_utils.load_examples(task_num=task_num)
+        code_golf_utils.verify_program(
+            task_num=task_num, examples=examples, task_path=task_path
+        )
