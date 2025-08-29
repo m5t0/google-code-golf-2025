2c6163fed3de6544d6948add31c6bf01642847c5
KazApps <kazuki_shogi@outlook.jp>
2025-08-29 15:58:38 +0900

    圧縮を意識した短縮

--- code/task378.py
+++ code/task378.py
@@ -1,13 +1,13 @@
 def p(g,r=range):
- d=0,1,0,-1;h=[*map(list,g)]
- for i in r(R:=len(g)):
-  for j in r(R):
-   if g[i][j]==0:continue
-   for k in r(4):
-    e=v=1
-    if R>i+d[k]>-1<i+d[(k+1)%4]<R>j+d[(k+1)%4]>-1<j+d[(k+2)%4]<R and g[i+d[k]][j+d[(k+1)%4]]<1>g[i+d[(k+1)%4]][j+d[(k+2)%4]]:
-     while(s:=j+e*(d[(k+1)%4]+d[(k+2)%4]))>-1<(t:=i+e*(d[k]+d[(k+1)%4]))<R>s:
-      if v>=2:h[t][s]=h[i][j]
-      if g[t][s]:v+=1
-      e+=1
- return h
+ d=0,1,0,-1;c=[*map(list,g)]
+ for i in r(b:=len(g)):
+  for j in r(b):
+   if g[i][j]!=0:
+    for k in r(4):
+     e=v=1
+     if b>i+d[k]>-1<i+d[(k+1)%4]<b>j+d[(k+1)%4]>-1<j+d[(k+2)%4]<b and g[i+d[k]][j+d[(k+1)%4]]<1>g[i+d[(k+1)%4]][j+d[(k+2)%4]]:
+      while(s:=j+e*(d[(k+1)%4]+d[(k+2)%4]))>-1<(t:=i+e*(d[k]+d[(k+1)%4]))<b>s:
+       if v>=2:c[t][s]=c[i][j]
+       if g[t][s]:v+=1
+       e+=1
+ return c
--- code/task387.py
+++ code/task387.py
@@ -1,12 +1,13 @@
 def p(g,r=range):
  u=0;n=len(g)
  for i in r(n):
-  if len(v:=[j for j,x in enumerate(g[i])if x%5])==2:
+  if len(v:=[j for j,c in enumerate(g[i])if c%5])==2:
    a,b=v
-   for k in 0,1,2,3,5,6,7,8:g[t:=i+k//3-1][a+k%3-1]=g[i][b];g[t][b+k%3-1]=g[i][a]
+   for c in r(9):
+    if c-4:g[i+c//3-1][a+c%3-1]=g[i][b];g[i+c//3-1][b+c%3-1]=g[i][a]
    for c in r(1,b-a):
-    if~min(c,b-a-c)&1:g[i][a+c]=5
+    if min(c,b-a-c)%2==0:g[i][a+c]=5
    if u<1:
     for c in r(1,u:=(w:=[x for x in r(n)if g[x][a]%5])[3]-w[1]):
-     if~min(c,u-c)&1:g[w[1]+c][a]=g[w[1]+c][b]=5
+     if min(c,u-c)%2==0:g[w[1]+c][a]=g[w[1]+c][b]=5
  return g
