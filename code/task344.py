097d108b15af16879ab39643f228ce88adb47dff
sktkmozt <kazu22kun@icolud.com>
2025-08-31 13:44:52 +0900

    task344-223bytes

--- code/task344.py
+++ code/task344.py
@@ -1,9 +1,8 @@
 def p(g,r=range):
  h,w=len(g),len(g[0]);d=0,1,0,-1,0
- q=[x[:]for x in g]
  for i in r(h):
   for j in r(w):
    if g[i][j]==3:
     for I in r(4):
-     if 0<=i+(a:=d[I])<h and 0<=j+(b:=d[I+1])<w and g[i+a][j+b]==2:q[i][j]=8;q[i+a][j+b]=0
- return q
+     if 0<=i+(a:=d[I])<h and 0<=j+(b:=d[I+1])<w and g[i+a][j+b]==2:g[i][j]=8;g[i+a][j+b]=0
+ return g
