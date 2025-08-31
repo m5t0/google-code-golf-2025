51af066dc9f6a721528a6aaceb9066e97f73345f
sktkmozt <kazu22kun@icolud.com>
2025-08-31 13:43:46 +0900

    task344-243bytes

--- code/task344.py
+++ code/task344.py
@@ -1,1 +1,9 @@
-p=lambda g,d=(0,1,0,-1,0),r=range:[[[[f,0][f==2and any(0<=i+d[k]<len(g)and 0<=j+d[k+1]<len(g[0])and g[i+d[k]][j+d[k+1]]==3for k in r(4))],8][f==3and any(0<=i+d[k]<len(g)and 0<=j+d[k+1]<len(g[0])and g[i+d[k]][j+d[k+1]]==2for k in r(4))]for j,f in enumerate(g[i])]for i in r(len(g))]
+def p(g,r=range):
+ h,w=len(g),len(g[0]);d=0,1,0,-1,0
+ q=[x[:]for x in g]
+ for i in r(h):
+  for j in r(w):
+   if g[i][j]==3:
+    for I in r(4):
+     if 0<=i+(a:=d[I])<h and 0<=j+(b:=d[I+1])<w and g[i+a][j+b]==2:q[i][j]=8;q[i+a][j+b]=0
+ return q
