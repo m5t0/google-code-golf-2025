7f955fbc8269a5d67d8c3a0297798f4c2237f456
sktkmozt <kazu22kun@icolud.com>
2025-09-01 16:56:47 +0900

    task369-225bytes

--- code/task369.py
+++ code/task369.py
@@ -1,3 +1,3 @@
 def p(g,e=enumerate):
- d=0,1,0,-1;f=lambda f,g,n,P:1+sum(f(f,g,(I,J),n)for i in range(4)if-1<(I:=n[0]+d[i])<10>(J:=n[1]+d[(i+1)%4])>=0and g[I][J]==0and(I,J)!=P)
+ d=0,1,0,-1,0;f=lambda f,g,n,P:1+sum(f(f,g,(I,J),n)for i in range(4)if-1<(I:=n[0]+d[i])<10>(J:=n[1]+d[i+1])>=0and g[I][J]==0and(I,J)!=P)
  return[[w or 4-f(f,g,(i,j),(i,j))for j,w in e(v)]for i,v in e(g)]
