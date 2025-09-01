e2da399f8c70b6f737a84e582a080e32051e8d91
moto <mo_to1015@yahoo.co.jp>
2025-09-01 15:40:29 +0900

    task340.pyの追加

---
+++ code/task340.py
@@ -0,0 +1,2 @@
+f=lambda v,j,m:v[0]*(j==1)*(v[0]in v[1:])+v[-1]*(j==m-2)*(v[-1]in v[:-1])
+p=lambda g,e=enumerate:[[[f(v,j,m:=len(g[0]))+f(w,i,n:=len(g)),v[j]][min(i,n-1-i,j,m-1-j)<1]for j,w in e(zip(*g))]for i,v in e(g)]
