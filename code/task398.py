a37244018c31836b9ce08e0cbe7a787e89fb7db8
sktkmozt <kazu22kun@icolud.com>
2025-09-01 12:24:06 +0900

    task398-97bytes

--- code/task398.py
+++ code/task398.py
@@ -1,6 +1,1 @@
-def p(g):
- g=g[0]
- s=5*sum(x!=0 for x in g)
- g=[0]*s+g+[0]*s
- r=range(s) 
- return[[g[i+j+1]for j in r]for i in r]
+def p(g):r=range(s:=5*len(set(g[0])-{0}));return[[([0]*s+g[0]+s*[0])[i+j+1]for j in r]for i in r]
