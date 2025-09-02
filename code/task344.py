3b4486d912445360782e8123c7dafe9ba1476a52
KazApps <kazuki_shogi@outlook.jp>
2025-09-02 18:42:18 +0900

    sktkmoztブランチのtask344.pyを短縮してマージ

--- code/task344.py
+++ code/task344.py
@@ -1,6 +1,5 @@
 def p(g,r=range):
- h,w=len(g),len(g[0]);d=0,1,0,-1,0
- for I in r(h*w):
-  for I in[*r(4)]*(g[i:=I//w][j:=I%w]==3):
-   if h>i+(a:=d[I])>-1<j+(b:=d[I+1])<w and g[i+a][j+b]==2:g[i][j]=8;g[i+a][j+b]=0
+ h,w=len(g),len(g[0]);d=0,1,0,-1
+ for K in r(h*w*4):
+  if h>(a:=(i:=K//4//w)+d[J:=K%4])>-1<(b:=(j:=K//4%w)+d[J-1])<w and g[a][b]==g[i][j]-1:g[i][j]=8;g[a][b]=0
  return g
