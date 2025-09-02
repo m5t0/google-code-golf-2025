3db1ce7beff3e01141142416b30ed1eddc06925d
KazApps <kazuki_shogi@outlook.jp>
2025-09-02 13:08:53 +0900

    sktkmoztブランチのtask354.pyを短縮してマージ

--- code/task354.py
+++ code/task354.py
@@ -1,6 +1,4 @@
 def p(g,r=range):
- d=0,1,0,-1,0
  def D(i,j,c):
-  if 0<=j<10>i and g[i][j]==5:g[i][j]=c;[D(i+d[I],j+d[I+1],c)for I in r(4)]
- [[D(i,j,s)for i in r(1,10)]for j in r(10)if(s:=g[0][j])]
- return g
+  if c>0<=j<10>i and g[i][j]==5:g[i][j]=c;[D(i+d[I],j+d[I-1],c)for I in r(4)]
+ d=0,1,0,-1;[D(I//10,j:=I%10,g[0][j])for I in r(99)];return g
