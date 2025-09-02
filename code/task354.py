206058980be8470c71c1d5c011366af3a1045c0f
sktkmozt <kazu22kun@icolud.com>
2025-09-02 12:14:24 +0900

    task354-176bytes

--- code/task354.py
+++ code/task354.py
@@ -1,6 +1,4 @@
 def p(g,r=range):
- d=0,1,0,-1,0
  def D(i,j,c):
-  if (0<=i<10)&(0<=j<10)and g[i][j]==5:g[i][j]=c;[D(i+d[I],j+d[I+1],c)for I in r(4)]
- [[D(i,j,g[0][j])for i in r(1,10)if g[i][j]==5]for j in r(10)if g[0][j]]
- return g
+  if c>0<=j<10>i and g[i][j]==5:g[i][j]=c;[D(i+d[I],j+d[I-1],c)for I in r(4)]
+ d=0,1,0,-1;[D(I//10,j:=I%10,g[0][j])for I in r(10,100)];return g
