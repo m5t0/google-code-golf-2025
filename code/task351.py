8a167e7106b0a01b594fc2084553a0b31b48d064
sktkmozt <kazu22kun@icolud.com>
2025-09-02 12:35:40 +0900

    task351-91bytes(短縮)

--- code/task351.py
+++ code/task351.py
@@ -1,1 +1,1 @@
-p=lambda g,r=range(16):[[g[15-i][15-j]for j in r if g[i][j]==3]for i in r if any(g[i][j]==3for j in r)]
+p=lambda g,r=range(16):[[g[15-i][15-j]for j in r if g[i][j]==3]for i in r if g[i].count(3)]
