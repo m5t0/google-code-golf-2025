a16b6bf6dd1a8c94b65005a02ee2abfd2c2af1dd
sktkmozt <kazu22kun@icolud.com>
2025-09-03 15:37:41 +0900

    task338-145bytes(短縮)

--- code/task338.py
+++ code/task338.py
@@ -1,6 +1,1 @@
-def p(g,r=range):
- d=0,1,0,-1,0;n,m=len(g),len(g[0])
- def D(i,j):
-  if (0<=i<n)&(0<=j<m)and g[i][j]<1:g[i][j]=3;[D(i+d[I],j+d[I+1])for I in r(4)]
- [[D(i+1,j+1)for i in r(n-1)if g[i][j+1]==g[i+1][j]==g[i][j]==2]for j in r(m-1)]
- return[[[x,0][x==2]for x in v]for v in g]
+def p(g):r=range(1,len(g)-1);[exec("g[i][j]=3")for i in r for j in r if g[i][j]<1<min(g[i-1][j],g[i][j-1])];return[[x*(x-2)for x in v]for v in g]
