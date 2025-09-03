50fa5b72e204de42403fc013db82ac34c7b4db91
sktkmozt <kazu22kun@icolud.com>
2025-09-03 19:40:01 +0900

    task331-159bytes(短縮)

--- code/task331.py
+++ code/task331.py
@@ -1,6 +1,6 @@
-def p(g,r=range):
- d=0,1,0,-1,0
- for x in r(100):
-  for k in [*r(4)]*(g[i:=x//10][j:=x%10]==1):
-   if 0<=(I:=i+d[k])<10>(J:=j+d[k+1])>=0:g[I][J]=[6,8,7,2][k]
+d=0,1,0,-1
+def p(g):
+ for x in range(100):
+  for k in[0,1,2,3]*(g[i:=x//10][j:=x%10]&1):
+   if-1<(I:=i+d[k])<10>(J:=j+d[k-1])>=0:g[I][J]=[7,8,6,2][k]
  return g
