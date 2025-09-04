9736f6a777b9bbd01d27f586ce7f54c9ea90ba1f
KazApps <kazuki_shogi@outlook.jp>
2025-09-05 01:00:48 +0900

    task061.pyを短縮

--- code/task061.py
+++ code/task061.py
@@ -1,6 +1,6 @@
 r=range
 def p(g):
  for s in r(1,n:=18):
-  v,f=[s*[0]for _ in r(s)],1
+  R=r(n);v,f=[s*[0]for _ in R],1
   for i in r(n*n):a,b=g[x:=i//n][y:=i%n],v[x%s][y%s];f-=a*b*(a!=b);v[x%s][y%s]=max(a,b)
-  if f>0:return[[v[i%s][j%s]for j in r(n)]for i in r(n)]
+  if f>0:return[[v[i%s][j%s]for j in R]for i in R]
