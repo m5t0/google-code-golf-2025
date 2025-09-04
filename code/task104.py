9a8253f4d35c7ba6dff62a6564b6920ffd7c4364
KazApps <kazuki_shogi@outlook.jp>
2025-09-04 14:46:20 +0900

    motoブランチのtask104.pyを短縮してマージ

---
+++ code/task104.py
@@ -0,0 +1,4 @@
+def p(g):
+ h=[[0]*9for _ in range(9)]
+ for k in range(64):h[(max(g[0])<1)+k//8][(max([*zip(*g)][0])<1)+k%8]=[g[0][2]or g[2][0],g[0][0]or g[2][2]][(k//8-3.5)*(k%8-3.5)>0]
+ return h
