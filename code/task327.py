759fc3f09148efa05056256e52946e5fcb0aa963
sktkmozt <kazu22kun@icolud.com>
2025-09-04 14:11:08 +0900

    task327-121bytes

--- code/task327.py
+++ code/task327.py
@@ -1,4 +1,4 @@
 def p(g):
  w=[[0]*6];g=w+[v+[0]*3for v in g]+w*3
- for i in range(6):g[i+1]=list(map(sum,zip([0]+g[i],g[i+1])))
+ for i in range(6):g[i+1]=[*map(sum,zip([0]+g[i],g[i+1]))]
  return g[1:]
