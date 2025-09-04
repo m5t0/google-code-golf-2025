de32a4e9f4e4d672875e154d6b47718f7a589e1e
sktkmozt <kazu22kun@icolud.com>
2025-09-04 21:40:33 +0900

    task314-102bytes

--- code/task314.py
+++ code/task314.py
@@ -1,1 +1,1 @@
-e=enumerate;m=min;p=lambda g:[[max(m(v[j%3::6]),m(w[i%3::6]),v[j])for j,w in e(zip(*g))]for i,v in e(g)]
+e=enumerate;p=lambda g:[[max(min(v[j%3::6]),min(w[i%3::6]),v[j])for j,w in e(zip(*g))]for i,v in e(g)]
