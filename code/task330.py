efe42bd03f62ebc2314650a3e5fdfa7f9256658b
KazApps <kazuki_shogi@outlook.jp>
2025-09-04 15:57:56 +0900

    sktkmoztブランチのtask330.pyを短縮してマージ

--- code/task330.py
+++ code/task330.py
@@ -1,9 +1,5 @@
-def p(g,r=range):
- d=0,1,0,-1,0;e={}
- def D(i,j,c):
-  if(0<=i<10>j>=0)^1|((i,j)in e):return 0
-  e[i,j]=c
-  if g[i][j]:
-   for I in r(4):g[i][j]+=D(i+d[I],j+d[I+1],c)
-  return g[i][j]
- return[[[D(i,j,(i,j)),1+(g[e[i,j][0]][e[i,j][1]]==30)][g[i][j]>0]for j in r(10)]for i in r(10)]
+d=0,1,0,-1
+def D(g,i,j,P):
+ if(0<=i<10>j>=0)^1or P-g[i][j]-1:return 0
+ g[i][j]+=1;return 1+sum(D(g,i+d[I],j+d[I-1],g[i][j])for I in[0,1,2,3])
+p=lambda g,e=enumerate:[[(w>0)+(D(g,i,j,w+(w>0))==6)for j,w in e(v)]for i,v in e(g)]
