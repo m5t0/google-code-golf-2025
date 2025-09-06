8894db9dc1e4c3f1f5e37cae67514ea2ecefd827
sktkmozt <kazu22kun@icolud.com>
2025-09-06 15:17:49 +0900

    task335-189bytes(短縮)

--- code/task335.py
+++ code/task335.py
@@ -1,5 +1,5 @@
-def p(g,e=enumerate):
- f=lambda n:[(i,j)for i,v in e(g)for j,w in e(v)if w==n][0];a,b=f(8);c,d=f(2)
+def p(g):
+ f=lambda n:[(g.index(v),v.index(w))for v in g for w in v if w==n][0];a,b=f(8);c,d=f(2)
  while a-c:a+=(a<c)-(a>c);g[a][b]=4
- while b-d:b+=(b<d)-(b>d);g[a][b]=4;g[c][d]=2
- return g
+ while b-d:b+=(b<d)-(b>d);g[a][b]=4
+ g[c][d]=2;return g
