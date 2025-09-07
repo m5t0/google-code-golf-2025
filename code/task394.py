7e0cb0fc0dcaabcecc58a74d529558720fd58d73
sktkmozt <kazu22kun@icolud.com>
2025-09-08 00:42:18 +0900

    task394-124bytes(短縮)

--- code/task394.py
+++ code/task394.py
@@ -1,3 +1,1 @@
-def p(g,r=range):
- for I in r((s:=len(g))*s):
-  if g[i:=I//s][j:=I%s]<1:x=g[i].count(0);return[[[y:=g[(i+k)%(z:=2+(s>6))][(j+l)%z],g[(i+k)%z+z][(j+l)%z]][y<1]for l in r(x)]for k in r(x)]
+p=lambda g,e=enumerate:[[g[i-[s:=2+(len(g)>6),-s][i<s]][j-[s,-s][j<s]]for j,w in e(zip(*g))if 0in w]for i,v in e(g)if 0in v]
