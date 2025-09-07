4f8e8cd89c5775d6e56363e52f27ed1c23cbdb0c
KazApps <kazuki_shogi@outlook.jp>
2025-09-07 16:57:46 +0900

    task160.pyを短縮

--- code/task160.py
+++ code/task160.py
@@ -1,2 +1,2 @@
 r=range
-p=lambda g:[exec("g[k//8+l//3][k%8+l%3]=2*((l//3-1)*(l%3-1)==0)")for k in r(64)if all(g[k//8+m//3][k%8+m%3]==(m!=4)for m in r(9))for l in r(9)]and g
+p=lambda g:any(exec("g[k//8+l//3][k%8+l%3]=2*(1in(l//3,l%3))")for k in r(64)if all(g[k//8+m//3][k%8+m%3]==(m!=4)for m in r(9))for l in r(9))or g
