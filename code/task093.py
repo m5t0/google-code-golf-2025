8f26534f30ed0e286df76997611a1324e9fff315
KazApps <kazuki_shogi@outlook.jp>
2025-09-07 21:44:39 +0900

    task093.pyを短縮

--- code/task093.py
+++ code/task093.py
@@ -1,3 +1,3 @@
 def p(g,r=range(14)):
- if 5in g[0]:return[*map(list,zip(*p([*zip(*g)])))]
+ if 5in g[0]:return[*zip(*p([*zip(*g)]))]
  a,b=min(s:=[i for i in r if 5in g[i]]),max(s);return[[(a-sum(map(bool,x[:a]))<=i<=b+sum(map(bool,x[b+1:])))*5for x in zip(*g)]for i in r]
