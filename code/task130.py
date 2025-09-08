c59088d8e0c7d71513c91b7363f8ba2b85121b69
KazApps <kazuki_shogi@outlook.jp>
2025-09-08 23:06:50 +0900

    sktkmoztブランチのtask130.pyを短縮してマージ

--- code/task130.py
+++ code/task130.py
@@ -1,2 +1,2 @@
-s=0,1,2
-p=lambda g:[[max((sum(v:=[a[3*j:3*j+3]for a in g[3*i:3*i+3]],[]).count(w:=v[k//3][k%3]),w)for k in range(9))[1]for j in s]for i in s]
+s=0,3,6
+p=lambda g:[[(g[i][j]-5or g[i][j+1]-5or g[i+1][j]-5)+5for j in s]for i in s]
