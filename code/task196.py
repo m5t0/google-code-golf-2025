8d53d3ebd9ac8590b0971858f399cd3321d0f5d1
KazApps <kazuki_shogi@outlook.jp>
2025-09-09 00:28:11 +0900

    motoブランチのtask196.pyを短縮してマージ

---
+++ code/task196.py
@@ -0,0 +1,1 @@
+def p(g,r=range):n=len(g);a=[*zip(*g)];[exec("g[i][k:l+1]=g[j][k:l+1]=[3]*(l+1-k)\nfor x in r(i,j):g[x][k]=g[x][l]=3")for i in r(n)for j in r(i+2,n)for k in r(n)for l in r(k+2,n)if(0in(g[i][k:l]+g[j][k:l]+[*a[k]][i:j]+[*a[l]][i:j]))^1];return g
