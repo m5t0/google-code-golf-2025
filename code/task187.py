26e873f6f8816ccf7d1e0483c70533340e230b87
sktkmozt <kazu22kun@icolud.com>
2025-09-09 00:13:26 +0900

    task187-207bytes(短縮)

---
+++ code/task187.py
@@ -0,0 +1,6 @@
+def p(g):
+ m,n=len(g),len(g[0]);g=[[b or 2for b in a]for a in g]
+ def f(i,j):
+  if n>abs(j)and abs(i)<m and g[i][j]==2:g[i][j]=3;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
+ for i in range(m):f(i,0);f(0,i)
+ return g
