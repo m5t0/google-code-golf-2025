2fd49b407ea658382f5678b1eed9482b8820369b
moto <mo_to1015@yahoo.co.jp>
2025-09-09 00:35:57 +0900

    task002.pyの短縮(191バイト)

--- code/task002.py
+++ code/task002.py
@@ -1,6 +1,6 @@
 def p(g):
  n=len(g);g=[[4-b%2for b in a]for a in g]
  def f(i,j):
-  if 0<=i<n>j>=0and g[i][j]>3:g[i][j]=0;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
- for i in range(n):f(i,0);f(i,n-1);f(0,i);f(n-1,i)
+  if-n<i<n>j>-n and g[i][j]>3:g[i][j]=0;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
+ for i in range(n):f(i,0);f(0,i);f(-1,i)
  return g
