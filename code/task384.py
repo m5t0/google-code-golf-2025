b63312c267803d82f5d4be8d601e27949443dd36
sktkmozt <kazu22kun@icolud.com>
2025-09-08 09:06:16 +0900

    task384-95bytes(短縮)

--- code/task384.py
+++ code/task384.py
@@ -1,4 +1,1 @@
-def p(g,e=enumerate,r=range):
- a,b=min(s:=[i for r in g for i,x in e(r)if x>0]),max(s)
- c,d=min(t:=[i for j in zip(*g) for i,x in e(j) if x>0]),max(t)
- return[[g[j//2+c][i//2+a]for i in r(2*(b-a+1))]for j in r(2*(d-c+1))]
+p=lambda g:[[w[g.index(v)]for w in zip(*g)for _ in"__"if 4in w]for v in g for _ in"__"if 4in v]
