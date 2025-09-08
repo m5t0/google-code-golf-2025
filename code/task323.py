10c575b4c8a8b95d3500e9a2f862b7d824054eda
KazApps <kazuki_shogi@outlook.jp>
2025-09-08 22:30:44 +0900

    task323.pyを短縮

--- code/task323.py
+++ code/task323.py
@@ -1,1 +1,1 @@
-def p(g,r=range(13)):_,a,b=max((g[i][j],i,j)for i in r for j in r);return[[g[i][j]or(abs(i-a+j-b-(i-a>0)+(i-a<0))<1+~(j-b)%2)*5for j in r]for i in r]
+def p(g,r=range(13)):_,a,b=max((g[i][j],i,j)for i in r for j in r);return[[g[i][j]or(abs(i-a+j-b+(i<a)-(i>a))<2-(j-b&1))*5for j in r]for i in r]
