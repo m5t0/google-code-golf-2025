3ab958bd0377e04a034488bb10f1f8792f7d4222
moto <mo_to1015@yahoo.co.jp>
2025-09-10 18:01:59 +0900

    task048.pyの追加

---
+++ code/task048.py
@@ -0,0 +1,7 @@
+def p(g,r=range):
+ m,n=len(g),len(g[0])
+ def f(i,j):
+  if m>i>-1<j<n and 0<g[i][j]<9:g[i][j]=9;f(i-1,j);f(i+1,j);f(i,j-1);f(i,j+1);
+ for k in r(m*n):
+  if g[k//n][k%n]==2:f(k//n,k%n);break
+ return[[all(all(w!=2for w in v)for v in g)*8]]
