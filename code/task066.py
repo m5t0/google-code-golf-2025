24162c58a39c45a3dd4883aabd59978b409999c1
moto <mo_to1015@yahoo.co.jp>
2025-09-10 22:27:38 +0900

    task066.pyの短縮(413バイト)

--- code/task066.py
+++ code/task066.py
@@ -1,7 +1,7 @@
 def p(g,e=enumerate):
- m,n=len(g),len(g[0]);h=[(i,j)for i,v in e(g)for j,w in e(v)if w==3][:2]
+ m,n=len(g),len(g[0]);h=[(i,j)for i,v in e(g)for j,w in e(v)if w==3]
  def f(i,j,x,y,c):
-  if(m>i>-1<j<n)^1or c>3:return 0
+  if(m>i>-1<j<n)^1or c>2:return 0
   if(v:=g[i][j]):return(v==2)+(v!=3)
   g[i][j]=3;b=f(i+x,j+y,x,y,c)
   if b>1:return 2
