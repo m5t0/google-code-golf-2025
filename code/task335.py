20d50ff2908a6329bf7ff95b6a19f0c04b2caf41
KazApps <kazuki_shogi@outlook.jp>
2025-09-11 21:53:37 +0900

    sktkmoztブランチのtask335.pyをマージ

--- code/task335.py
+++ code/task335.py
@@ -1,5 +1,5 @@
 def p(g):
- f=lambda n:[(g.index(v),v.index(w))for v in g for w in v if w==n][0];a,b=f(8);c,d=f(2)
+ f=lambda n:divmod(sum(g,[]).index(n),len(g[0]));a,b=f(8);c,d=f(2)
  while a-c:a+=(a<c)-(a>c);g[a][b]=4
- while b-d:b+=(b<d)-(b>d);g[a][b]=4
- g[c][d]=2;return g
+ while b-d:g[a][b]=4;b+=(b<d)-(b>d)
+ return g
