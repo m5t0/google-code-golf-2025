fb26e835cce7b560764429653d647f2b7a633df2
KazApps <kazuki_shogi@outlook.jp>
2025-09-11 21:27:23 +0900

    task002.pyを短縮

--- code/task002.py
+++ code/task002.py
@@ -1,6 +1,5 @@
 def p(g):
- n=len(g)
  def f(i,j):
   if-n<i<n>j>-n and g[i][j]<1:g[i][j]=4;[f(i+k,j)==f(i,j+k)for k in[-1,1]]
- for i in range(n):f(i,0);f(0,i);f(-1,i)
+ for i in range(n:=len(g)):f(i,0);f(0,i);f(-1,i)
  return[[x+2^6for x in v]for v in g]
