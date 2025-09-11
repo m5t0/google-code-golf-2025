100d9a9940789448075d70230dbf09d49c9f9c75
KazApps <kazuki_shogi@outlook.jp>
2025-09-12 06:01:09 +0900

    task378.pyを短縮

--- code/task378.py
+++ code/task378.py
@@ -1,9 +1,9 @@
-def p(g,r=range):
+def p(g):
  d=0,1,0,-1;c=eval(str(g))
- for I in r((b:=len(g))*b*4):
-  if g[i:=I//4//b][j:=I//4%b]*(b>i+d[k:=I%4]>-1<i+d[a:=-~k&3]<b>j+d[a]>-1<j+d[u:=-~a&3]<b and g[i+d[k]][j+d[a]]<1>g[i+d[a]][j+d[u]]):
+ for I in range((b:=len(g))*b*4):
+  if g[i:=I//4//b][j:=I//4%b]*(b>i+(z:=d[k:=I%4])>-1<i+(x:=d[a:=-~k&3])<b>j+x>-1<j+(y:=d[-~a&3])<b and g[i+z][j+x]<1>g[i+x][j+y]):
    e=v=1
-   while(s:=j+e*(d[a]+d[u]))>-1<(t:=i+e*(d[k]+d[a]))<b>s:
+   while(s:=j+e*(x+y))>-1<(t:=i+e*(z+x))<b>s:
     if v>1:c[t][s]=c[i][j]
     v+=g[t][s];e+=1
  return c
