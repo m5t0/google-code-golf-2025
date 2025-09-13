bd967f85903c446d396b00818f1a76f6dcefd9ae
KazApps <kazuki_shogi@outlook.jp>
2025-09-13 21:08:29 +0900

    motoブランチのtask117.pyを短縮してマージ

--- code/task117.py
+++ code/task117.py
@@ -1,6 +1,6 @@
 def p(g,e=enumerate):
- x,y=[(i,j)for i,v in e(g)for j,w in e(v)if w*(j<len(g)-3>i>3)and all(g[i+k//3][j+k%3]==w*(1-(k//3+k%3)%2)for k in range(9))][0]
+ x,y=[(i,j)for i,r in e(g)for j,v in e(r)if v*(j<len(g)-3>i>3)and all(g[i+k//3][j+k%3]==v*(1-(k//3+k%3)%2)for k in range(9))][0]
  for i,v in e(g):
   for j,w in e(v):
-   for k,l in(w and w!=g[x][y])*[(s:=2*x-i+2,j),(i,t:=2*y-j+2),(s,t)]:g[k][l]=w
+   if w and w-g[x][y]:g[a:=2*x-i+2][j]=g[i][b:=2*y-j+2]=g[a][b]=w
  return g
