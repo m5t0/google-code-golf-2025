4e0ac6594ee6dfa85cf2f707ff2a5c83170705b2
KazApps <kazuki_shogi@outlook.jp>
2025-09-15 00:43:18 +0900

    sktkmoztブランチのtask240.pyをマージ

---
+++ code/task240.py
@@ -0,0 +1,1 @@
+def p(g,e=enumerate):g=[[max(w[i],w[~i],v[~j],g[~i][~j])for j,w in e(zip(*g))]for i,v in e(g)];return[[w[i]or sum(y&1and(19-x>y>x)*a[x-17]+(19-x<y<x)*a[x-2]for x,y,a in((i,j,v),(j,i,w)))for j,w in e(zip(*g))]for i,v in e(g)]
