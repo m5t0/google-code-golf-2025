f1d0858fb27550cea09549ee5d2977b0e66c71f9
KazApps <kazuki_shogi@outlook.jp>
2025-09-14 20:25:57 +0900

    task335.pyを短縮

--- code/task246.py
+++ code/task246.py
@@ -1,5 +1,5 @@
 def p(g):
  f=lambda n:divmod(sum(g,[]).index(n),len(g[0]));a,b=f(3);c,d=f(2)
- while a-c:g[a:=a+(c>a or-1)][b]=8
- while b-d:g[a][b]=8;b+=d>b or-1
+ while a-c:g[a:=a+(a<c or-1)][b]=8
+ while b-d:g[a][b]=8;b+=b<d or-1
  return g
--- code/task335.py
+++ code/task335.py
@@ -1,5 +1,5 @@
 def p(g):
  f=lambda n:divmod(sum(g,[]).index(n),len(g[0]));a,b=f(8);c,d=f(2)
- while a-c:a+=(a<c)-(a>c);g[a][b]=4
- while b-d:g[a][b]=4;b+=(b<d)-(b>d)
+ while a-c:g[a:=a+(a<c or-1)][b]=4
+ while b-d:g[a][b]=4;b+=b<d or-1
  return g
