76a5bf8cb79ddbfa8bd67beb42ae4e24f79cca2e
sktkmozt <kazu22kun@icolud.com>
2025-09-14 18:26:32 +0900

    task245-183bytes

--- code/task245.py
+++ code/task245.py
@@ -1,2 +1,3 @@
-f=lambda g:[*map(lambda v:2in v,g)].index(1)
-p=lambda g,e=enumerate:[[(w>2)*3or(g[i+f(g)-(x:=sum(g,[]).index(3))//(l:=len(v))-1][j+f(zip(*g))-x%l-1]==2)*2for j,w in e(v)]for i,v in e(g)]
+e=enumerate
+f=lambda g:[i for i,v in e(g)if 2in v][0]
+p=lambda g:[[(w>2)*3or(g[i+f(g)-(x:=sum(g,[]).index(3))//(l:=len(v))-1][j+f(zip(*g))-x%l-1]==2)*2for j,w in e(v)]for i,v in e(g)]
