f19a3b0deec9681eaa435e805c60ad24b0b9c39f
KazApps <kazuki_shogi@outlook.jp>
2025-09-15 22:57:53 +0900

    task334.pyを短縮

--- code/task334.py
+++ code/task334.py
@@ -1,1 +1,2 @@
-p=lambda g,r=(0,1,2):[[5*((i==r[2-(x:=max(sum(g,[])))])|(j==(x+1)//2))for j in r]for i in r]
+r=0,1,2
+p=lambda g:[[5*((i==r[2-(x:=max(sum(g,[])))])|(j==x+1>>1))for j in r]for i in r]
