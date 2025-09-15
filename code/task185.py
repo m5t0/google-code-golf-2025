58d337000357e57048d86f11cdc918a6bc17a429
KazApps <kazuki_shogi@outlook.jp>
2025-09-15 22:37:16 +0900

    motoブランチのtask185.pyを短縮してマージ

---
+++ code/task185.py
@@ -0,0 +1,2 @@
+t=0,1,2
+p=lambda g:[[(len(s:={[[(x:=w[g.index(v)])*(x!=w[0])for w in zip(*g)if len({*w}-{0})>1]for v in g if len({*v}-{0})>1][i+l//2][j+l%2]for l in[*t,3]})==1)*sum(s)for j in t]for i in t]
