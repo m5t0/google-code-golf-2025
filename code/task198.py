a5b3dc3c07d21072d93f450beccc2e3127786ca5
moto <mo_to1015@yahoo.co.jp>
2025-09-15 18:54:09 +0900

    task198.pyの追加

---
+++ code/task198.py
@@ -0,0 +1,7 @@
+def p(g):
+ r=range;n=len(g);R=r(n);m=[i+1for i in R if g[0][i]or g[i][2]][0];s=n//m+1;h=[s*[3]for _ in r(s)]
+ for k in r(s-1):
+  for i in R:
+   for x,y,z,v,s,t in(l:=m*-~k-1,i,k,i//m,1,0),(i,l,i//m,k,0,1):
+    if g[x][y]<1:g[x][y]=4;exec("if-~i%m:h[z][v]=h[z+s][v+t]=4")
+ return[[g[i][j]or h[i//m][j//m]for j in R]for i in R]
