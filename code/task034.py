6ab885ec401ab156e4856bc01316ae045f05f9d1
KazApps <kazuki_shogi@outlook.jp>
2025-09-17 01:24:29 +0900

    task034.pyを短縮

--- code/task034.py
+++ code/task034.py
@@ -1,1 +1,3 @@
-def p(g,r=range):s=r(9);f=lambda x:min(i for i in s if any(x[i]));x,y=f(g),f([*zip(*g)]);return[[max((v:=g[x+k//2][y+k%2])*(v!=2)for k in r(4))*max((g[t:=x+k//2][u:=y+k%2]==2)*(2>((w:=(i-t)//(k//2*2-1))-(j-u)//(k%2*2-1))>=-1<w)for k in r(4))or g[i][j]for j in s]for i in s]
+r=range(9)
+f=lambda x:min(i for i in r if any(x[i]))
+p=lambda g:[[max((v:=g[(x:=f(g))+k//2][(y:=f([*zip(*g)]))+k%2])*(v!=2)for k in r[:4])*max((g[t:=x+k//2][u:=y+k%2]==2)*(2>((w:=(i-t)//(k//2*2-1))-(j-u)//(k%2*2-1))>=-1<w)for k in r[:4])or g[i][j]for j in r]for i in r]
