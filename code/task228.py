01b26ef58f55895b3623cdad20c2328f267e7c1c
KazApps <kazuki_shogi@outlook.jp>
2025-09-17 00:32:20 +0900

    task228.pyを短縮

--- code/task228.py
+++ code/task228.py
@@ -1,4 +1,4 @@
 def p(g):
- I,J=divmod(X:=[*map(bool,V:=sum(g,[]))].index(1),10)
- for t,s in(a:=g[I].count(x:=V[X]),b:=V[X::10].count(x)),(-1,b),(a,-1),(-1,-1):g[I+s][J+t]=g[A:=I+b+1-s-(s<0)*4][B:=J+a+1-t-(t<0)*4];g[A][B]=0
+ X=[*map(bool,V:=sum(g,[]))].index(1);J=X%10
+ for t,s in(a:=g[I:=X//10].count(x:=V[X]),b:=V[X::10].count(x)),(-1,b),(a,-1),(-1,-1):g[I+s][J+t]=g[A:=I+b+1-s-(s<0)*4][B:=J+a+1-t-(t<0)*4];g[A][B]=0
  return g
