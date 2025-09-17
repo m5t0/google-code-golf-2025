fb2808d671b59bc2f26e938e4539b46af5778f6c
KazApps <kazuki_shogi@outlook.jp>
2025-09-17 19:39:17 +0900

    task344.pyを短縮

--- code/task344.py
+++ code/task344.py
@@ -1,5 +1,5 @@
-def p(g,r=range):
+def p(g):
  h,w=len(g),len(g[0]);d=0,1,0,-1
- for K in r(h*w*4):
+ for K in range(h*w*4):
   if h>(a:=(i:=K//4//w)+d[J:=K%4])>-1<(b:=(j:=K//4%w)+d[J-1])<w and g[a][b]==g[i][j]-1:g[i][j]=8;g[a][b]=0
  return g
