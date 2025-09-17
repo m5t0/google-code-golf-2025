d2e2b5a2987f8c8fa5802179ff657d881ab48d77
KazApps <kazuki_shogi@outlook.jp>
2025-09-17 18:57:33 +0900

    task250.pyを短縮

--- code/task250.py
+++ code/task250.py
@@ -1,3 +1,3 @@
-r=range
-f=lambda I:{I-1:r(I),I:[I],I+1:[I+1],I+2:r(I+2,10)}
-p=lambda g:[[i in(X:=f((x:=sum(g,[]).index(2))//10))and j in(Y:=f(x%10))and max(g[x][y]for x in X[i]for y in Y[j])for j in r(10)]for i in r(10)]
+r=range(10)
+f=lambda I:{I-1:r[:I],I:[I],I+1:[I+1],I+2:r[I+2:]}
+p=lambda g:[[i in(X:=f((x:=sum(g,[]).index(2))//10))and j in(Y:=f(x%10))and max(g[x][y]for x in X[i]for y in Y[j])for j in r]for i in r]
