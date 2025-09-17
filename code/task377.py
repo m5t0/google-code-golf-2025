4689c15af97f5d91babe3f24b37a6b72030c9159
KazApps <kazuki_shogi@outlook.jp>
2025-09-17 22:12:24 +0900

    task377.pyを短縮

--- code/task377.py
+++ code/task377.py
@@ -1,3 +1,1 @@
-def p(g,r=range,w=()):
- for u in g:w=max(u[:1]+[u[j+1]for j in r(len(u)-1)if u[j]-u[j+1]],w,key=len);s=len(w)
- return[[w[min(i,j,s+~j,s+~i)]for i in r(s)]for j in r(s)]
+def p(g,r=range):s=len(w:=max((v[:1]+[v[j+1]for j in r(len(v)-1)if v[j]-v[j+1]]for v in g),key=len));return[[w[min(i,j,s+~j,s+~i)]for i in r(s)]for j in r(s)]
