414e07f127eb205041367845f907506ab4b1ccf6
KazApps <kazuki_shogi@outlook.jp>
2025-09-19 00:43:09 +0900

    sktkmoztブランチのtask277.pyをマージ

--- code/task277.py
+++ code/task277.py
@@ -1,5 +1,5 @@
 def p(g,r=range(10)):
  def D(i,j,P):
-  if{i,j}<{*r}and P==g[i][j]^1:g[i][j]=P;return-~sum(D(i-1+I//3,j-1+I%3,P)for I in r[:9])
-  return 0
+  if{i,j}-{*r}or P!=g[i][j]^1:return 0
+  g[i][j]=P;return-~sum(D(i-1+I//3,j-1+I%3,P)for I in r[:9])
  a={D(i,j,9)for j in r for i in r}-{0};return[[((s:=g[i][j])>2)+(D(i,j,s^1)==min(a))for j in r]for i in r]
