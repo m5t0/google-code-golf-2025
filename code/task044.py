9bd2fa23afd47ff4e5894618d8e2e5a63ace85c9
moto <mo_to1015@yahoo.co.jp>
2025-09-19 10:49:43 +0900

    task044.pyの短縮

--- code/task044.py
+++ code/task044.py
@@ -1,10 +1,6 @@
 def p(g,r=range,e=enumerate):
- a,*l=sum(g,[]),;n=10;s=[h for c in r(1,n)if c!=5if(h:=[i for i in r(100)if a[i]==c])if all((a[i-(i>0):i+2]+a[i-n*(i>9):i+11:n]).count(c)>2for i in h)]
+ a,*l=sum(g,[]),;n=10;s=[[i for i,v in e(a)if v==c]for c in r(n)]
  for i,v in e(g):
-  if(5in v)>i/9and 0in{*v[(x:=(o:=str(v)[1::3]).find("5")+1):(y:=o.rfind("5"))]}:l+=[j for j in r(i*n+x,i*n+y)if a[j]<1]
-  elif l:
-   for b in s:
-    if len(b)==len(l)and all(k-b[0]==l[m]-l[0]for m,k in e(b)):
-     for m in b:a[m-b[0]+l[0]]=a[m];a[m]=0
-   l=[]
+  if(5in v)>i/9and 0in v[(x:=v.index(5)+1):(y:=9-v[::-1].index(5))]:l+=[k for j in r(x,y)if a[k:=i*n+j]<1]
+  else:[a==exec("a[m-b[0]+l[0]]=a[m];a[m]=0")for b in s if[m-b[0]for m in b]==[m-l[0]for m in l]for m in b];l=[]
  return[a[i*n:][:n]for i in r(n)]
