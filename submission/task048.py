a89380608136b6ebd229da87d426ca809df706a7
KazApps <kazuki_shogi@outlook.jp>
2025-09-20 20:45:57 +0900

    motoブランチのtask048.pyを短縮してマージ

--- submission/task048.py
+++ submission/task048.py
@@ -4,4 +4,4 @@ def p(g):
   for k in[-1,1]*(m>i>-1<j<n and 0<g[i][j]<9):g[i][j]=9;f(i+k,j);f(i,j+k)
  for k in range(m*n):
   if g[k//n][k%n]==2:f(k//n,k%n);break
- return[[8*('2'not in str(g))]]
+ return[[8*('2'not in"%s"%g)]]
