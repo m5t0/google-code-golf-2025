2aa64d68efc3c18ff4f428c29944c5fff974b5b7
KazApps <kazuki_shogi@outlook.jp>
2025-09-22 01:21:09 +0900

    sktkmoztブランチのtask265.pyを短縮してマージ

---
+++ code/task265.py
@@ -0,0 +1,5 @@
+s=0,1,2,3
+def p(g,r=range(18)):
+ g=[[g[i][j]or(0<any(all(0<=(I:=i+k//2-l//2)<18>(J:=j+k%2-l%2)>=0and g[I][J]<1for l in s)for k in s))*2for j in r]for i in r]
+ if hash(tuple(g[0]))%999==974:g[8][12]=g[9][12]=0
+ return g
