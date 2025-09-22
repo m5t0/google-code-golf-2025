ff4a9a75b37ea11f83760c5454ae178d610de7c9
sktkmozt <kazu22kun@icolud.com>
2025-09-23 07:06:54 +0900

    task246-151bytes(短縮)

--- code/task246.py
+++ code/task246.py
@@ -1,5 +1,4 @@
 def p(g):
- f=lambda n:divmod(sum(g,[]).index(n),len(g[0]));a,b=f(3);c,d=f(2)
- while a-c:a+=(a<c)-(a>c);g[a][b]=8
- while b-d:g[a][b]=8;b+=(b<d)-(b>d)
+ (a,b),(c,d)=(divmod(sum(g,[]).index(x),len(g[0]))for x in[3,2]);g[c][b:d:b<d or-1]=[8]*abs(b-d)
+ while a-c:g[a:=a+(a<c or-1)][b]=8
  return g
