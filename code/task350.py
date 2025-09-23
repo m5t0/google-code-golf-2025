e549856405b966f62fa79c057a0cb2e59c01df69
sktkmozt <kazu22kun@icolud.com>
2025-09-24 05:34:33 +0900

    task350-111bytes(短縮)

--- code/task350.py
+++ code/task350.py
@@ -1,1 +1,1 @@
-p=lambda g,e=enumerate:[[8if((a:=[k for k,x in e(v)if x])and a[0]<j<a[-1]and v[j]<1)or((b:=[k for k,x in e(w)if x])and b[0]<i<b[-1]and w[i]<1)else v[j]for j,w in e(zip(*g))]for i,v in e(g)]
+p=lambda g,e=enumerate:[[w[i]or(1in{*w[i:]}&{*w[:i]}|{*v[:j]}&{*v[j:]})*8for j,w in e(zip(*g))]for i,v in e(g)]
