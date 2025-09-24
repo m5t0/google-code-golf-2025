696bffca28075a087b649b180c73679211ae3730
moto <mo_to1015@yahoo.co.jp>
2025-09-24 22:17:55 +0900

    task002.pyの短縮(173バイト)

--- code/task002.py
+++ code/task002.py
@@ -1,5 +1,5 @@
 def p(g):
  def f(i,j):
-  for k in[-1,1]*(g[i:=i%n][j:=j%n]<1):g[i][j]=4;f(i+k,j);f(i,j+k)
+  for k in[-1,1]*(g[i%n][j%n]<1):g[i%n][j%n]=4;f(i+k,j);f(i,j+k)
  for i in range(n:=len(g)):f(i,0);f(0,i);f(-1,i)
  return[[x+2^6for x in v]for v in g]
