feafdd162eba7be2c965c0a94aaf634bf02d4115
KazApps <kazuki_shogi@outlook.jp>
2025-09-25 18:56:59 +0900

    task265.pyを短縮

--- submission/task265.py
+++ submission/task265.py
@@ -1,6 +1,5 @@
-n=17
 def p(g):
- for i in range(n*n):
-  for v in(a:=g[i//n:][:2])*all(max(v[i%n:][:2])<5for v in a):v[i%n]=v[i%n+1]=2
+ for i in range(289):
+  for v in(a:=g[i//17:][:2])*all(max(v[i%17:][:2])<5for v in a):v[i%17]=v[i%17+1]=2
  if hash((*g[0],))%999==974:g[8][12]=g[9][12]=0
  return g
