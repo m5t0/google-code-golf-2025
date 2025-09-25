92dd27d7bf3ccc589d1f66293e4b0bae6b33712e
KazApps <kazuki_shogi@outlook.jp>
2025-09-25 22:42:29 +0900

    bonoさんによるtask002.pyの短縮をマージ

--- code/task002.py
+++ code/task002.py
@@ -1,4 +1,3 @@
-def p(g):
- for v in g[1:-1]:v[1:-1]=[x or 4for x in v[1:-1]]
- for _ in g*4:g=[*zip(*eval(str(g).replace('0, 4','0,0'))[::-1])]
+def p(g,r=str.replace):
+ for _ in(g:=r(str(g),*'04'))*4:*g,=zip(*eval(r(r(str(g),'0, 4','0,0'),'(4','(0'))[::-1])
  return g
