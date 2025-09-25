d8986559a4f9f6f921c70e8821206c3b0568cb5e
KazApps <kazuki_shogi@outlook.jp>
2025-09-25 14:04:22 +0900

    bonoさんのtask002.pyを短縮してマージ

--- submission/task002.py
+++ submission/task002.py
@@ -1,5 +1,4 @@
 def p(g):
- def f(i,j):
-  for k in[-1,1]*(g[i%n][j%n]<1):g[i%n][j%n]=4;f(i+k,j);f(i,j+k)
- for i in range(n:=len(g)):f(i,0);f(0,i);f(-1,i)
- return[[x+2^6for x in v]for v in g]
+ for v in g[1:-1]:v[1:-1]=[x or 4for x in v[1:-1]]
+ for _ in g*4:g=[*zip(*eval(str(g).replace('0, 4','0,0'))[::-1])]
+ return g
