2b323c4dcaf8fceafce17653b74b3e0538ad6a58
KazApps <kazuki_shogi@outlook.jp>
2025-09-25 22:05:47 +0900

    task102.pyを短縮

--- submission/task102.py
+++ submission/task102.py
@@ -1,5 +1,5 @@
 def p(g,r=range):
- h=[*zip(*g)]
+ *h,=zip(*g)
  for k in r(144):
   for l in r(2,12-max(x:=k//12,y:=k%12)):
    for v in(min(g[x][y:(w:=y+l)]+g[z:=x+l][y:w]+[*h[y][x:z]+h[w][x:z]])>4>max(sum((g[i][y+1:w]for i in r(x+1,z)),[])))*g[x+1:z]:v[y+1:w]=[2]*(l-1)
