b17e5b89c40ecf0bd2f277d25c72312dfe6b1184
KazApps <kazuki_shogi@outlook.jp>
2025-09-27 00:00:03 +0900

    task002.pyと同じようにtask187.pyを短縮

--- submission/task187.py
+++ submission/task187.py
@@ -1,6 +1,1 @@
-def p(g):
- n=len(g[0]);g=[[b or 2for b in a]for a in g]
- def f(i,j):
-  if(i<len(g))*(n>j>-n)and g[i][j]==2:g[i][j]=3;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
- for i in range(n):f(i,0);f(0,i)
- return g
+p=lambda g,r=str.replace:[g:=eval(r(str(g),*'02')),exec("g[:]=zip(*eval(r(r(str(g),'3, 2','3,3'),'(2','(3'))[::-1]);"*56)][0]
