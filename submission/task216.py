63e01ec2cc9e3d8d77e29aeddf12540597d722b9
KazApps <kazuki_shogi@outlook.jp>
2025-09-27 21:13:21 +0900

    task216.pyを短縮

--- submission/task216.py
+++ submission/task216.py
@@ -1,2 +1,2 @@
 f=lambda v:v[:[*v,0].index(0)]
-p=lambda g,e=enumerate:max(([f(g[i+I][j:])for I,_ in e(f(w[i:]))]for j,w in e(zip(*g))for i,_ in e(g)),key=lambda g:str(g).count('2'))
+p=lambda g,e=enumerate:max((str(s:=[f(g[i+I][j:])for I,_ in e(f(w[i:]))]).count('2'),-i-j,s)for j,w in e(zip(*g))for i,_ in e(g))[2]
