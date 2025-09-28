f2481feb0b07c89ebfee63743aa0b303d684e586
KazApps <kazuki_shogi@outlook.jp>
2025-09-28 14:48:46 +0900

    task007.pyを短縮

--- code/task007.py
+++ code/task007.py
@@ -1,1 +1,2 @@
-r=range;p=lambda g:[[max(g[k//7][k%7]*((k/7+k%7-i-j)%3<1)for k in r(49))for j in r(7)]for i in r(7)]
+r=range
+p=lambda g:[[max(g[k//7][k%7]for k in r((i+j)%3,49,3))for j in r(7)]for i in r(7)]
