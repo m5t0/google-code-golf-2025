34601852da9ad7e9a91d0917aac565009c88dc63
sktkmozt <kazu22kun@icolud.com>
2025-09-29 07:00:02 +0900

    task014-83bytes(短縮)

--- code/task014.py
+++ code/task014.py
@@ -1,4 +1,1 @@
 p=lambda g:[[w[g.index(v)]for w in zip(*g)if len({*w})>2]for v in g if len({*v})>2]
-
-f=lambda g:filter(lambda v:len({*v})>2,g)
-p=lambda g:[[w[g.index(v)]for w in[*f(zip(*g))]]for v in[*f(g)]]
