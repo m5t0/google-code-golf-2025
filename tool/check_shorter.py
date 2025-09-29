f384e252bb5042438f6e0222a72f4745308fd782
KazApps <kazuki_shogi@outlook.jp>
2025-09-29 10:52:08 +0900

    check_shorter.pyでsubmissionディレクトリも見るように変更した

--- tool/check_shorter.py
+++ tool/check_shorter.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 
+
 def check(master_files, dev_branch_files, dev_branch_name):
     ok = True
 
@@ -15,6 +16,12 @@ def check(master_files, dev_branch_files, dev_branch_name):
         ok = False
 
     for name in sorted(set(mnames) & set(dbnames)):
+        if os.path.isfile(
+                mreplaced := os.path.join(os.path.dirname(mnames[name]), "../submission", name)) and os.path.isfile(
+                dbreplaced := os.path.join(os.path.dirname(dbnames[name]), "../submission", name)):
+            mnames[name] = mreplaced
+            dbnames[name] = dbreplaced
+
         try:
             with open(mnames[name]) as fm, open(dbnames[name]) as fb:
                 mlen = len(fm.read())
@@ -30,19 +37,27 @@ def check(master_files, dev_branch_files, dev_branch_name):
     if ok:
         print(f"✅ {dev_branch_name}: OK")
 
+
 def main():
     master_branch = "master"
     dev_branches = ["moto", "sktkmozt", "bono"]
     master_files = glob("./code/*.py")
 
     for dev_branch in dev_branches:
-        os.system(f"git checkout {dev_branch} && git pull && cp -r ./code ./{dev_branch}")
+        os.system(f"git checkout {dev_branch} && git pull")
+
+        shutil.copytree("./code", f"./{dev_branch}/code")
+
+        if os.path.isdir("./submission"):
+            shutil.copytree("./submission", f"./{dev_branch}/submission")
+
         os.system(f"git checkout {master_branch}")
 
-        dev_files = glob(f"./{dev_branch}/*.py")
+        dev_files = glob(f"./{dev_branch}/code/*.py")
         check(master_files, dev_files, dev_branch)
 
         shutil.rmtree(f"./{dev_branch}")
+
 
 if __name__ == "__main__":
     main()
