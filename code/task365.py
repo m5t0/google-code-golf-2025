1e307bbd4b67a2048499c3348e4000490db244fb
sktkmozt <kazu22kun@icolud.com>
2025-09-30 05:53:15 +0900

    task365-163bytes(短縮)

--- code/task365.py
+++ code/task365.py
@@ -1,10 +1,2 @@
-def p(g,r=range):
- I=J=p=u=e=0
- for i in r(len(g)):
-  for j in r(len(g[0])):
-   c=d=f=0
-   while i+c<len(g)and g[i+c][j]:c+=1
-   while j+d<len(g[0])and g[i][j+d]:d+=1
-   f=sum(g[i+k][j+l]==2for k in r(c)for l in r(d))
-   if f>e:I=i;J=j;p=c;u=d;e=f
- return[[g[i+I][j+J]for j in r(u)]for i in r(p)]
+f=lambda v:v[:[*v,0].index(0)]
+p=lambda g,e=enumerate:max((str(s:=[f(g[i+I][j:])for I,_ in e(f(w[i:]))]).count('2'),-i-j,s)for j,w in e(zip(*g))for i,_ in e(g))[2]
