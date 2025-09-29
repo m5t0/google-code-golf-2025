653f0f8876ad6bebcd8a0be7563072546eeb0a78
KazApps <kazuki_shogi@outlook.jp>
2025-09-29 09:12:27 +0900

    task180.pyを短縮

--- submission/task185.py
+++ submission/task185.py
@@ -1,2 +1,2 @@
 t=0,1,2
-p=lambda g:[[(len(s:={[[x*(x!=w[0])for x,w in zip(v,zip(*g))if len({*w}-{0})>1]for v in g if len({*v}-{0})>1][i+l//2][j+l%2]for l in[*t,3]})==1)*sum(s)for j in t]for i in t]
+p=lambda g:[[(len(s:={[[w[0]*(w[0]!=w[1])for w in zip(v,*g)if len({*w}-{0})>1]for v in g if len({*v}-{0})>1][i+l//2][j+l%2]for l in[*t,3]})==1)*sum(s)for j in t]for i in t]
