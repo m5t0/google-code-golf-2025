0a672876e3d6dbc0f947db921bc23c4eb094017b
KazApps <kazuki_shogi@outlook.jp>
2025-10-01 16:45:22 +0900

    bonoブランチのtask366.pyを短縮してマージ

---
+++ submission/task366.py
@@ -0,0 +1,3 @@
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes("eR…®Ã ý¢ƒ–>Ò°¹¹Ix+m˜@saúõ¯0÷ÊQ¬³=9	5èµõœÄ¼ÜàEë5T3òµ‹Ir’'7òŠ„€¶½Åâ…,·l™Ï¿['IÇ@Év€@ì1bÎ3rB‚zDP®ìQ¯Íæ¯3ŒTŽŠ6ñ¸ÝL€bú´Gi.‘…•¾ÓÅ>~,ÂÖ'ÍÂv«7æð¨tïÊìÁ.øì6‚¤RmQxåëˆ:V`p&Y¯¹ƒT¼rÀÈ-‚ÑúXfcrÆj2ÜßÈøÁr+„ñ»·=qê9vU³*ÝU}cëïú;š8¥l¸£¿òK6ßooæØÇŠtwÆÇÆ]Ïó!‡^u¥4\×êmQ^&^Åæ²èü(´°%›¶äó®TåH9ùLý","L1"),-9))
