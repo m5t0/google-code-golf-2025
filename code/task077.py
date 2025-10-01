93fb036cd11173cc7b127e7226b12f1c6ac446a2
Joonha Kwon <joonhakwon@JoonhanoMacBook-Pro.local>
2025-10-02 02:24:42 +0900

    Solve task077

---
+++ code/task077.py
@@ -0,0 +1,7 @@
+def p(g,r=range):
+ for _ in r(88):
+  for x in r(len(g)-1):
+   for y in r(len(g[0])-1):
+    if (g[x][y]!=0)&(g[x][y]!=2)&(g[x+1][y]in[2,4])&(((g[x][y+1]in[2,4])or((x-1>-1)&(g[x-1][y]in[2,4])))):g[x][y]=4
+  g=[*map(list,(zip(*g)))][::-1]
+ return g
