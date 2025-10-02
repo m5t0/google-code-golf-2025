1573d44453c1694b17b07e1c80c21438d5289dfe
moto <mo_to1015@yahoo.co.jp>
2025-10-03 00:50:03 +0900

    task158.pyの追加

---
+++ submission/task158.py
@@ -0,0 +1,4 @@
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes("""QAnÃ ü
+9TamÒØu-yB?€8`œ¨	X¤´¯ïbµŠ%R;bg´Ã°Æ’‰:à^œõ…:Y)öi¾D><÷>¹­eBb} šuÌIÎ7µ‚LH‘ ÝhèÉ ›—…?Üðè—¥£%¹M¤©åXNÛm£ä¡œž•'§ëä;(€}³¯v~%Zd|Côª•4rqÒçnÐdžuX=VñÍå ŠTÊnÁh¤6'`‘æê…E€Õo¼žc‚¦HÅÌ>¼3ê¯ÛUŠÏ·±P…”¹˜ž®ïˆü_®"ÒÂJ|~*®`½ì-]^bF‚ÖlS?X7–õÃ•ÃoSm01GÜ""","L1"),-9))
