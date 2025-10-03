b01b72b8dab900dff609193b721eb025400cfbc0
KazApps <kazuki_shogi@outlook.jp>
2025-10-04 01:08:42 +0900

    sktkmoztブランチのtask075.pyを短縮してマージ

--- submission/task075.py
+++ submission/task075.py
@@ -1,1 +1,1 @@
-p=lambda g,r=range(9):[exec("g[i-1+k//3][j+3+k%3]=g[k//3][k%3]")for i in r for j in r for k in[*r]*(g[i][j+4]==1)]and g
+p=lambda g,r=range(9):[g[i][:4]+[g[i-i%3+1][j-j%3+5]*g[i%3][j%3]for j in r]for i in r]
