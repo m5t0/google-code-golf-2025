9f5dbb94946bc86f1022206718e6fc464ab541dc
KazApps <kazuki_shogi@outlook.jp>
2025-10-03 15:51:45 +0900

    bonoブランチのtask077.pyを短縮してマージ

---
+++ submission/task077.py
@@ -0,0 +1,4 @@
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes("""MËA
+Ã …á}N1KM"Tq%x‘¨Š™¦`zújÒ†ì†ïŸ÷p2	#jœRpTuà„;ÄH„l°K9dv‰Êxó#l—`nöl}…2pk6“£´´oÔ€‰óø?/Øˆí­ëG¡ê÷¯e´éŸS&s|­ã'fÒJ­QªN:@·¾1Aø""","L1"),-9))
