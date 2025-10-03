83dd429ab5acf2d956a5c0b95c257fc8c33adf9a
KazApps <kazuki_shogi@outlook.jp>
2025-10-03 23:02:41 +0900

    sktkmoztブランチのtask243.pyをマージ

--- submission/task243.py
+++ submission/task243.py
@@ -1,4 +1,1 @@
-def p(g):
- def D(i,j,o=1):
-  for k in[-1,1]*(n>i>-1<j<n>1>o*g[i][j]):g[i][j]=1;D(i+k,j);D(i,j+k)
- n=len(g);[g[i//n][i%n]-1or D(i//n,i%n,0)for i in range(n*n)];return g
+p=lambda g:exec("g[:]=zip(*eval(str(g).replace('1, 0','1,1'))[::-1]);"*80)or g
