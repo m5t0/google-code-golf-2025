505c3c266a05818cb545410fd2488622e30aa47f
sktkmozt <kazu22kun@icolud.com>
2025-10-03 21:13:19 +0900

    task344-77bytes(短縮)

--- code/task344.py
+++ code/task344.py
@@ -1,5 +1,1 @@
-def p(g,r=range):
- h,w=len(g),len(g[0]);d=0,1,0,-1
- for K in r(h*w*4):
-  if h>(a:=(i:=(I:=K//4)//w)+d[J:=K%4])>-1<(b:=(j:=I%w)+d[J-1])<w and g[a][b]==g[i][j]-1:g[i][j]=8;g[a][b]=0
- return g
+p=lambda g:exec("g[:]=zip(*eval(str(g).replace('3, 2','8,0'))[::-1]);"*4)or g
