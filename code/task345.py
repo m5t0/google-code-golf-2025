a9fa15f9f5e57140876e3c463334dcb9161548d7
sktkmozt <kazu22kun@icolud.com>
2025-10-03 21:31:52 +0900

    task345-127bytes(短縮)

--- code/task345.py
+++ code/task345.py
@@ -1,3 +1,1 @@
-def p(g):
- def D(i,j,c):g[i][j]=2;x=i+d[c];y=j+d[c+1];D(i+d[e:=c^1],j+d[e+1],e)if c|(j<9>i>0and g[x][y])else 0>x or D(x,y,c)
- d=-1,0,1,0;[D(9,i,0)for i in range(10)if g[9][i]];return g
+p=lambda g,r=range(10):[[g[i][j]or(({2}<={*(Z:=[*zip(*g)])[j][i:]}<={0,2})+({*Z[j-1][i-(i>0):]}>{0,5}))*2for j in r]for i in r]
