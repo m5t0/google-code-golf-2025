54df6114f9093ad50a70d4804df3646acf740172
KazApps <kazuki_shogi@outlook.jp>
2025-10-03 13:39:27 +0900

    sktkmoztブランチのtask061.pyをマージ

--- code/task061.py
+++ code/task061.py
@@ -1,6 +1,1 @@
-r=range
-def p(g):
- for s in r(1,n:=18):
-  R=r(n);v,f=[s*[0]for _ in R],1
-  for i in r(n*n):a,b=g[x:=i//n][y:=i%n],v[x%s][y%s];f-=a*b*(a!=b);v[x%s][y%s]=max(a,b)
-  if f>0:return[[v[i%s][j%s]for j in R]for i in R]
+p=lambda g,r=range(18):[[i*j%max(g[-1])+1for j in r]for i in r]
