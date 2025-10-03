61c533c9eb5d37a736aee06bfa4f769b2a25d078
sktkmozt <kazu22kun@icolud.com>
2025-10-03 22:24:20 +0900

    task359-67bytes(短縮)

--- code/task359.py
+++ code/task359.py
@@ -1,3 +1,1 @@
-def p(g):
- if c:=max((g[0]).count(k)for k in g[0])<len(g[0])*0.6:g=[*map(list,zip(*g))]
- return[x:=[[max((v.count(k),k)for k in v)[1]]*len(v)for v in g],[*map(list,zip(*x))]][c]
+p=lambda g:[[max(V:=v+[*w],key=V.count)for w in zip(*g)]for v in g]
