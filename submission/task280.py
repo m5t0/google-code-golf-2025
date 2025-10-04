7f12a93ad0568951d8f5a36492ad19414b72863c
KazApps <kazuki_shogi@outlook.jp>
2025-10-04 22:51:03 +0900

    bonoブランチのtask280.pyを短縮してマージ

---
+++ submission/task280.py
@@ -0,0 +1,3 @@
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes("%ÁŠÃ †ïyŠ9jŒ°f÷dvöEdX5ÖÚ –B}öšÌíãû†ù/~]„!cžSðÒvpóIÙÁrÏð1A?G9Ed‘Áf6Ú°ƒ¸@p‘ÜJˆã£2¤g¨˜ô:±·„µw#M¿¸>¯qó0þrWïB¶(4½Â{EÚvÒ:i¬™ÏßÔtýmÞÅK^q}’œµÚ´œ}}äá","L1"),-9))
