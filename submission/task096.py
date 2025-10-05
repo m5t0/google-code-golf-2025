82999f2429e7bbd228e05a11d2309b16e9f1c9d2
Joonha Kwon <joonhakwon@JoonhanoMacBook-Pro.local>
2025-10-05 23:13:00 +0900

    Solve task096.py

---
+++ submission/task096.py
@@ -0,0 +1,4 @@
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes("""…QÝªœ0¾?O‘ÞeÖØ£Ò«Èìs,¡¸[\rõ,‰GCéÃ7‰¡+…Ò+g¾ŸùÆÉ~`O®…EÛ-ºùÆH†\r–ffy]ŽÆ}ÎAK
+T; Q%êJ¬*j×H?Ô[2~?Œßâ´lÐ±;žú…?\00Ì:¤1‘ã‹Ü &o4*Ä¥Z‡VIˆN:1álÞ˜;>™âSéŠ®8Uêˆ`ìþWHðYÎø’3n˜ÜÙÈ;")é±ÊP?¹>´1ñeÌ¿CQ?êÒÁ¥)üŸ<mQ««‡_üvÍ;Ì0ú6Cxt™»'ŸŒ[ÅOóäMR–µˆçýê>ìÊ¡ÕØoÝÄÝj9Ñ@§F©K¨š8êUüãQN¯0@YƒÜ…?&›p1"ßKïïÈS6­¦QÑ(ÇbWHIcoÅi_œˆx@ŸNö÷ÿþ¦í×O»0ý""","L1"),-9))
