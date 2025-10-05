93afd1b038104b4765554d9b63637806c7db6340
sktkmozt <kazu22kun@icolud.com>
2025-10-06 05:31:45 +0900

    task396-275bytes(短縮)

--- code/task396.py
+++ code/task396.py
@@ -1,14 +1,1 @@
-def p(g,r=range):
- d={}
- for v in g:
-  for x in v:d[x]=d.get(x,0)+1
- s=min(d,key=d.get)
- c=w=l=e=0
- for i in r(L:=len(g)):
-  for j in r(R:=len(g[0])):
-   if (n:=g[i][j])>0!=n-s:
-    o=p=0
-    while o+i<L and g[i+o][j]==n:o+=1
-    while j+p<R and g[i][j+p]==n:p+=1
-    if o*p>l*e:c,w,l,e=i,j,o,p
- return[[[0,s][g[c+i][w+j]>0]for j in r(e)]for i in r(l)]
+def p(g,e=enumerate):M=min(V:=sum(g,[]),key=V.count);a=max(([v[j:J+1]for v in g[i:I+1]]for i,_ in e(g)for j,_ in e(g[0])for I,_ in e(g)for J,_ in e(g[0])if{sum({*V})-M}=={*g[i][j:J+1],*[*zip(*g)][j][i:I+1]}),key=lambda g:len(sum(g,[])));return[[x and M for x in v]for v in a]
