5f489f338d06f17235d4edc40a4821ed355f5c5f
KazApps <kazuki_shogi@outlook.jp>
2025-10-06 13:55:50 +0900

    motoブランチのtask319.pyを短縮してマージ

---
+++ submission/task319.py
@@ -0,0 +1,3 @@
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes('UQÛ’‚0}÷+ºO&ˆ;øŠ†?P? ÓÙé@©¸ˆ—ëøïÛ]Ü§&çœœ¤I¦rvñ‚jºèÜ*ºÈLø­F2Ÿéµ«ZÜ–ÄS–_k–²¢bµ¬´‚M„EÎÒRLVÙÄ±˜e¥uf’8ç*ˆRaÉ Gÿ7th¼‹zÇ2ãBÍ^ ÛÌZôŞ¶÷İ}øeÃe-}Mípù_0ŸŸş&ñiñ6ÿºTdÂ!&jDôB¯<¿+a?I2	´Çg…OŸEndY–ĞÄä~ÜÅ¤y±²?¯F!xGt€{‘ÈZYjv)ˆ®ß0÷›æòè8£{·¨Cb-“õfgMwGI´k¦#Aû+Şƒæ±¾«"½î-Æµj»ºbò',"L1"),-9))
