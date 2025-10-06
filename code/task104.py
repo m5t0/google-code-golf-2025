d4f22f4cdc601023b902a08507a0743f3931ad73
KazApps <kazuki_shogi@outlook.jp>
2025-10-06 21:08:46 +0900

    bonoブランチのtask104.pyを短縮してマージ

--- code/task104.py
+++ code/task104.py
@@ -1,4 +1,6 @@
 def p(g):
- h=[[0]*9for _ in range(9)]
- for k in range(64):h[(max(g[0])<1)+k//8][(max(next(zip(*g)))<1)+k%8]=[g[0][2]or g[2][0],g[0][0]or g[2][2]][(k//8-3.5)*(k%8-3.5)>0]
- return h
+ a=[s:=[3]*4+[0]*5]*4+[[0]*4+s[:5]]*4+[[0]*9]
+ for k in g[0][0],g[0][2],g[2][2],g[2][0]:
+  if k:break
+  *a,=zip(*a[::-1])
+ return a
