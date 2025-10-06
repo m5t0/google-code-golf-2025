e9b2ff49095006c73b7ea4611a554eaca4af350f
KazApps <kazuki_shogi@outlook.jp>
2025-10-06 14:01:56 +0900

    task319.pyを短縮

--- code/task319.py
+++ code/task319.py
@@ -1,8 +1,8 @@
 def p(g):
- v=sum(g,[]);e=max(v,key=v.count);l=[c for c in range(10)if c!=e and c in v]
+ v=sum(g,[]);e=max(v,key=v.count);l=sorted({*v}-{e})
  for c in l:
   a=[[[e,x][x==c]for*w,x in zip(*g,v)if c in w]for v in g if c in v];d=[[w for w in r for _ in'00']for r in a for _ in'00']
   for h in l:
    for i in range(1-len(d),(N:=len(g))):
     for j in range(1-(M:=len(d[0])),(O:=len(g[0]))):
-     if all(s:=[[e,u:=g[i+x][j+y]][u==h]*((u==h)==(d[x][y]==c))for x in range(len(d))for y in range(M)if N>i+x>-1<j+y<O])>0<s.count(t:=sum({*s}-{e}))==v.count(t):return a
+     if all(s:=[[e,u:=g[i+x][j+y]][u==h]*((u==h)==(d[x][y]==c))for x in range(len(d))for y in range(M)if N>i+x>-1<j+y<O])>0<s.count(h)==v.count(h):return a
--- submission/task319.py
+++ submission/task319.py
@@ -1,3 +1,3 @@
 #coding:L1
 import zlib
-exec(zlib.decompress(bytes('UQÛ’‚0}÷+ºO&ˆ;øŠ†?P? ÓÙé@©¸ˆ—ëøïÛ]Ü§&çœœ¤I¦rvñ‚jºèÜ*ºÈLø­F2Ÿéµ«ZÜ–ÄS–_k–²¢bµ¬´‚M„EÎÒRLVÙÄ±˜e¥uf’8ç*ˆRaÉ Gÿ7th¼‹zÇ2ãBÍ^ ÛÌZôÞ¶÷Ý}øeÃe-}Mípù_0ŸŸþ&ñiñ6ÿºTdÂ!&jDôB¯<¿+a?I2	´Çg…OŸEndY–ÐÄä~ÜÅ¤y±²?¯F!xGt€{‘ÈZYjv)ˆ®ß0÷›æòè8£{·¨Cb-“õfgMwGI´k¦#Aû+Þƒæ±¾«"½î-Æµj»ºbò',"L1"),-9))
+exec(zlib.decompress(bytes("UOËrÂ0¼ó¾!Ã„k@üðO'“&')Ã¿×2´´¾XÚÕîJ¹9‰XLfÂQÛ_ÁJ¥qcèšŽàä§™È­²º¯:Ü”ÔÖMgr¸Gî±¼›ÎÄ©nD&ŠJ”ÞA¤¤”2rÔj$Ê´'£AŽL7ˆ¬tXœžó³Â1gÅèô&÷C°˜kBùáËyÏƒ¦a<ýûl¶;36	mÒÊX/KSAŽ	qiùhÿXxù;	ûçH®b^q|)BûñÊiYB›ø}ì²ªXøÃ/‹IkÕuÀ?A®<5i¢‘óÆwÞs¯€Not>à°ó–»åzëM·G»xÛ®²º¯:`W÷['éú¦é7","L1"),-9))
