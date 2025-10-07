f821a3954fe38249d8f5305d01e2a66766dcff8a
moto <mo_to1015@yahoo.co.jp>
2025-10-07 23:56:57 +0900

    task219.pyの追加

---
+++ submission/task219.py
@@ -0,0 +1,5 @@
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes("""­PAnÃ ¼óŠ½Tb–LÚT
+
+ùBÄÁqœÈÄ‘Ú×%§j=1šefÖù#Üh‹’€U:Àñ:BàøaºøÑÞ}‡#Øá“>ÐˆÊê:½NiÁõš¿Ù:-X“‘(ˆ@£âu¼{Giœ.´M“³Ñ½QªÕ!ÃŽõ&»³×h‡ÖS‡™èBÔU—½ŸVÞ]·è6…™ãbS¹9x‰ W_áFWé/˜3˜þ“†SD´ƒÛ÷ÿµ”•0²ñDéª\\kƒñe»Ý*õ¶~—Ò´æBðJ a\rƒÒi-[|ä¦Ù¦‘`.XÌÔ¡(â¢rLp‡¸Y¦t0ýJ_æðG‡Šv»çyöKMöªþqù„Õr¯}FŸÆÚo""","L1"),-9))
