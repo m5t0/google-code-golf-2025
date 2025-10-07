9afe2fb7f4ec4c650d6bb17bc337bd6a86724c9d
KazApps <kazuki_shogi@outlook.jp>
2025-10-08 00:59:18 +0900

    format

--- tool/compcheck.py
+++ tool/compcheck.py
@@ -211,9 +211,11 @@ def process_task(task_num, state):
         if (cache := state.get_cache(task_num)) and cache[0] == get_hash(our_code.encode()):
             compressor_used = None
             improvement = cache[1]
-            output_lines.append(f"our code already cached" + (f" (saved {improvement} bytes)" if improvement > 0 else ""))
+            output_lines.append(
+                f"our code already cached" + (f" (saved {improvement} bytes)" if improvement > 0 else ""))
         else:
-            improvement, compressor_used, output_str = process_code_single(task_num, "our", our_code, "\033[92m", out, True)
+            improvement, compressor_used, output_str = process_code_single(task_num, "our", our_code, "\033[92m", out,
+                                                                           True)
             output_lines.append(output_str)
             if improvement > 0:
                 output_lines.append(f"Wrote to {out} (saved {improvement} bytes)")
