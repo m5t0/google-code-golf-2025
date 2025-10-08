67b68146bd2fd796eb268775bff87bf5cccfc4ab
KazApps <kazuki_shogi@outlook.jp>
2025-10-08 22:03:16 +0900

    sktkmoztブランチのtask234.pyをマージ

--- code/task234.py
+++ code/task234.py
@@ -1,3 +1,1 @@
-def p(g):
- for _ in' '*4:u=[r for r in g if r.count(max([*filter(any,g)][0]))-1];g=[*zip(*(len(g)-len(u))*g[:1]+u)][::-1]
- return g
+p=lambda g:[g:=[*zip(*(len(g)-len(u:=[v for v in g if~-v.count(max([*filter(any,g)][0]))]))*g[:1]+u)][::-1]for _ in[0]*4][3]
