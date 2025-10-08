0a51d3e30fce8bc01a14dbc1ef3852ef08f124fe
sktkmozt <kazu22kun@icolud.com>
2025-10-08 10:30:22 +0900

    task218-92bytes(短縮)

--- code/task218.py
+++ code/task218.py
@@ -1,2 +1,1 @@
-f=lambda g:sorted({j for j in range(20)for v in g if(v[j-1]-v[j])*v[j]})
-p=lambda g:[[g[i][j]for j in f(g)]for i in f([*zip(*g)])]
+p=lambda g:[*zip(*(f:=lambda g:[b for a,b in zip([[]]+g,g)if(a!=b)*any(b)])([*zip(*f(g))]))]
