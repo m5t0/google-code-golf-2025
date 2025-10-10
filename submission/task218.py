0864620fb1a5cfb0819250308bd13890cce9a0d5
KazApps <kazuki_shogi@outlook.jp>
2025-10-10 23:15:52 +0900

    Lucienブランチのtask218.pyをマージ

--- submission/task218.py
+++ submission/task218.py
@@ -1,2 +1,1 @@
-f=lambda g:sorted({j for j in range(20)for v in g if(v[j-1]-v[j])*v[j]})
-p=lambda g:[[g[i][j]for j in f(g)]for i in f([*zip(*g)])]
+p=lambda g,D=dict.fromkeys,F=filter,Z=zip:[*Z(*D(Z(*D(Z(*F(any,Z(*F(any,g))))))))]
