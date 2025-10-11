5d1de0a4aff2ec82926a61fbba599b3e3f729955
KazApps <kazuki_shogi@outlook.jp>
2025-10-11 23:10:58 +0900

    Lucienブランチのtask017.pyを短縮してマージ

--- submission/task017.py
+++ submission/task017.py
@@ -1,6 +1,1 @@
-r=range
-def p(g):
- for s in r(1,n:=21):
-  R=r(n);v,f=[s*[0]for _ in R],1
-  for i in r(n*n):a,b=g[x:=i//n][y:=i%n],v[x%s][y%s];f-=a*b*(a!=b);v[x%s][y%s]=max(a,b)
-  if f>0:return[[v[i%s][j%s]for j in R]for i in R]
+p=lambda n:(s:=range(21),l:=min(i for i in s[2:]if all((a-b)*a*b<1for a,b in zip(n[0],n[0][i:]))),n:={(a%l,b%l):v for a in s for b in s if(v:=n[a][b])})and[[n[i%l,j%l]for j in s]for i in s]
