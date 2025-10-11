e48eeffa7298e0abc0a620ac377a887ed2c60aae
Joonha Kwon <joonhakwon@JoonhanoMacBook-Pro.local>
2025-10-11 17:23:21 +0900

    Improve task190.py

--- submission/task190.py
+++ submission/task190.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec(r"g[:]=zip(*eval(re.sub(r'0(?=.{34}(?:(?:.{35})*)([^0]).{34}\1.{31}\1, \1)',r'\1',str(g)))[::-1]);"*4)or g
+p=lambda g:exec(r"g[:]=zip(*eval(re.sub(r'0(?=.{34}(?:.{35})*([^0]).{34}\1.{31}\1, \1)',r'\1',str(g)))[::-1]);"*4)or g
