4e5df4d77c6ff40f865d93f6c9c3943a9c837a50
KazApps <kazuki_shogi@outlook.jp>
2025-10-11 18:55:41 +0900

    sktkmoztブランチのtask092.pyをマージ

--- code/task092.py
+++ code/task092.py
@@ -1,2 +1,1 @@
-f=lambda w,i:((b:=max(w[:i+1]))==max(w[i:]))*b
-p=lambda g,e=enumerate:[[v[j]or f(w,i)or f(v,j)for j,w in e(zip(*g))]for i,v in e(g)]
+p=lambda g,e=enumerate:[[sum({*w[:i+1]}&{*w[i:]})or(sum({*v[:j+1]}&{*v[j:]}))for j,w in e(zip(*g))]for i,v in e(g)]
