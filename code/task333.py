2d2e99172300d434c6059cb058353341a2e30b1e
sktkmozt <kazu22kun@icolud.com>
2025-10-11 11:40:54 +0900

    task333-116bytes(短縮)

--- code/task333.py
+++ code/task333.py
@@ -1,2 +1,2 @@
-f=lambda v,j:(3in v[:j])*sum(v[j:])+(3in v[j:])*sum(v[:j])
-p=lambda g,e=enumerate:[[w[i]or f(v,j)+f(w,i)for j,w in e(zip(*g))]for i,v in e(g)]
+import re
+p=lambda g:exec("g[:]=zip(*eval(re.sub(r'3, ([0,\s]*)0, ([1-9])',r'3,\\1\\2,\\2',str(g)))[::-1]);"*28)or g
