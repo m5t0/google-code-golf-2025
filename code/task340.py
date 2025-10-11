a87120c7e27bc138865abf7563ade9c159fdd43e
KazApps <kazuki_shogi@outlook.jp>
2025-10-11 22:32:52 +0900

    sktkmoztブランチのtask340.pyを短縮してマージ

--- code/task340.py
+++ code/task340.py
@@ -1,7 +1,5 @@
-def p(g,e=enumerate):
- for i,v in e(g[1:-1],1):
-  for j,w in e(v[1:-1],1):
-   if w:v[j]=0
-   for a,b,c,d in(i,0,i,1),(i,-1,i,-2),(0,j,1,j),(-1,j,-2,j):
-    if g[a][b]==w:g[c][d]=w
- return g
+def p(g):
+ for _ in[0]*4:
+  for v in g[1:-1]:v[1]=v[0]*(v[0]in v[1:])
+  *g,=map(list,zip(*g[::-1]))
+ return g[:2]+[v[:2]+[0]*(len(v)-4)+v[-2:]for v in g[2:-2]]+g[-2:]
