54c2fa806c45e7d44de2cb2e126b48ca0784f5a3
sktkmozt <kazu22kun@icolud.com>
2025-10-12 18:30:39 +0900

    task286-151bytes(短縮)

--- code/task286.py
+++ code/task286.py
@@ -1,4 +1,1 @@
-def p(g):
- def D(i,j,P,o=1):
-  for k in[-1,1]*((m>j>-1<i<n)*P%8>=1>g[i][j]*o):g[i][j]=P;D(i+k,j,x:=sum({*sum(g,[])})-8-P);D(i,j+k,x)
- n=len(g);m=len(g[0]);[D(i%n,I:=i//n,g[i%n][I],0)for i in range(n*m)];return g
+def p(g,r=str.replace):n,m={*sum(g,[])}-{0,8};return[g:=[*zip(*eval(r(r(str(g),f'{n}, 0',f'{n},{m}'),f'{m}, 0',f'{m},{n}'))[::-1])]for _ in[0]*200][-1]
