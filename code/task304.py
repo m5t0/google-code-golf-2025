bb6ef6d05111ecf7fd155b6494be7c443914a1db
Luis Vielva <LuisVielva@gmail.com>
2025-10-13 22:28:56 +0200

    304

--- code/task304.py
+++ code/task304.py
@@ -1,1 +1,1 @@
-p=lambda g,r=range(9):[[g[i//3][j//3]==max([*r,9],key=sum(g,[]).count)and g[i%3][j%3]for j in r]for i in r]
+p=lambda g,r=range(9):[[g[i%3][j%3]*(g[i//3][j//3]==max(s:=sum(g,[]),key=s.count))for j in r]for i in r]
