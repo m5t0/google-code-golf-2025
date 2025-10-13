172039a1c6daeda53348beec65e3b0718a333c88
KazApps <kazuki_shogi@outlook.jp>
2025-10-14 00:46:29 +0900

    089

--- code/task089.py
+++ code/task089.py
@@ -1,6 +1,6 @@
 def p(r):
- f=[[(d//13,d%13)for d in range(169)if r[d//13][d%13]==e]for e in range(169)];u={e:((p,a),[(i,d-p,(t-a)*(2*e-5))for d,t in f[i]])for e in(2,3)for i in range(169)for p,a in f[e]if{(p+i//3-1,a+i%3-1)for i in range(9)if i-4}&{*f[i]}}
+ i=[[(o//13,o%13)for o in range(169)if r[o//13][o%13]==e]for e in range(169)];u={e:((g,a),[(d,o-g,(n-a)*(2*e-5))for o,n in i[d]])for e in(2,3)for d in range(169)for g,a in i[e]if{(g+d//3-1,a+d%3-1)for d in range(9)if d-4}&{*i[d]}}
  for e in u:
-  for p,a in f[e]:
-   for i,d,t in u[e][1]*((p,a)!=u[e][0]):r[d+p][t+a]=i
+  for g,a in i[e]:
+   for d,o,n in u[e][1]*((g,a)!=u[e][0]):r[o+g][n+a]=d
  return r
