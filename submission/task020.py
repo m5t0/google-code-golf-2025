a2ffbf12696545a3a84a0871b447e63e43f002cf
KazApps <kazuki_shogi@outlook.jp>
2025-10-14 02:04:47 +0900

    020, 023, 051, 058, 089, 101, 102

--- submission/task020.py
+++ submission/task020.py
@@ -1,3 +1,3 @@
 #coding:L1
 import zlib
-exec(zlib.decompress(bytes("-!@Ø¯P×ÄÀ®¾$a¹‰ÁLç¾þª=Â›g¦÷ÆbšE ˆÒõ¼Ðí›©ÀñKÍ>¬èJÕ¡ÄÐx×È ÷(ŽŠQç6#±¦g>J_SxåC#“XvG‹êùüÜÌýÏãY8ƒKw ¡—+žƒ‚úd-Üó¬C	}©åh:‚hÇÔs¢gëÏ€©æÄy”¯®bð","L1"),-9))
+exec(zlib.decompress(bytes("-!@Ø¯P×ÄÀ®¾$a¹‰ÁÌelœ¾þª=Â›g¦÷ÆbªE ˆÒõ¼Ðí›©À±SµkºòÑ¡ÄÐx×È ÷(ŽŠQç6#±¦g>Z_SxåC#“XvG‰êùüÜÌýÏãY8ƒKw ¡—+žƒ‚úd-ÜëZèK-GÓ”@;¦žå8[L5'Î£¬x5ƒ?","L1"),-9))
