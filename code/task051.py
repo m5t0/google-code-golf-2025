74c7efc6d9f4fa5cd892ce63216f991a6110fc56
moto <mo_to1015@yahoo.co.jp>
2025-10-15 00:29:03 +0900

    shorten task051(171 bytes)

--- code/task051.py
+++ code/task051.py
@@ -1,4 +1,1 @@
-def p(u,r=range):
- d=sum(u,[]);d,y=divmod(d.index(min(d,key=d.count)),i:=len(u[0]))
- for f in r(9):
-  if(n:=f//3-1,t:=f%3-1)!=(0,0)and n*t==0and u[d-n][y-t]<1:return[[u[m][f]or((m-d)*t==(f-y)*n and(n and(m-d)//n>0 or t and(f-y)//t>0))*u[d][y]for f in r(i)]for m in r(len(u))]
+p=lambda g:exec("g[:]=[*zip(*[[v[j]or(len({*v})>2)*(c:=min(v,key=v.count))*(v[j:].count(c)==1)*(sum(v[v.index(c)+1:])<1)for j in range(len(v))]for v in g])][::-1];"*4)or g
