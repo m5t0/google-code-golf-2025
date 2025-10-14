67ac426f1ec047ff94335d72453c3d1021fb1e4d
sktkmozt <kazu22kun@icolud.com>
2025-10-14 15:11:04 +0900

    task139-101bytes(短縮)

--- submission/task139.py
+++ submission/task139.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec("g[:]=zip(*eval(re.sub('0(?=(..[47].{25}[47]))','7',str(g)))[::-1]);"*12)or g
+p=lambda g:exec("g[:]=zip(*eval(re.sub('0(?=..[47].{25}[47])','7',str(g)))[::-1]);"*12)or g
