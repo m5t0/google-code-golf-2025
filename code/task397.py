f28c97054496c201324aa062ca36f95ebf3e84b1
Luis Vielva <LuisVielva@gmail.com>
2025-10-15 08:59:14 +0200

    397

--- code/task397.py
+++ code/task397.py
@@ -1,4 +1,5 @@
-def p(g,r=range):
- for I in r(81)[::-1]:
-  for k in[*r(len(s:={g[i:=I//9+k//2][j:=I%9+k%2]for k in r(4)}))]*min(s):g[t:=i-~k][j-1]=g[t][j]=3
+def p(g,I=81):
+ while I:
+  I-=1;j=I%9
+  for k in[*range(len(s:={*g[i:=I//9][j:j+2],*g[i+1][j:j+2]}))]*min(s):g[i+2+k][j:j+2]=3,3
  return g
