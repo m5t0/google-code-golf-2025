a20a1bcaf4d0310bf9b5f7b951fecd0d43151ecd
Joonha Kwon <joonhakwon@JoonhanoMacBook-Pro.local>
2025-10-15 18:33:46 +0900

    Solve task051

--- code/task051.py
+++ code/task051.py
@@ -1,4 +1,2 @@
-def p(p):
-	d=len(p);o=len(p[0]);f=sum(p,[]);l=min({*f}-{0},key=f.count);i,e=divmod(f.index(l),o);q,c=next((f,l)for(f,l)in((0,1),(1,0),(0,-1),(-1,0))if(0<=i+f<d)*(0<=e+l<o)*p[i+f][e+l]<1)
-	while(-1<(i:=i-q)<d)&(-1<(e:=e-c)<o):p[i][e]=p[i][e]or l
-	return p
+import re
+p=lambda g:exec(r"g[:]=zip(*eval(re.sub(r'0(?=(, 0)*, ([^0])(, \2)*, ((?!\2)[^0]), 0)',r'\4',str(g)))[::-1]);"*4)or g
