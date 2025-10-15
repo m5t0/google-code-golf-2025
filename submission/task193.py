e6ece8d695417644dd6b4d97aa8033706a10d6ce
Luis Vielva <LuisVielva@gmail.com>
2025-10-15 14:43:48 +0200

    193 198

--- submission/task193.py
+++ submission/task193.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec("g[:]=zip(*eval(re.sub('0, [^0](, 0|\))',r'0,0\\1',str(g)))[::-1]);"*8)or g
+p=lambda g:exec("g[:]=zip(*eval(re.sub(r'0, .(?=, 0|\\))','0,0',str(g)))[::-1]);"*8)or g
