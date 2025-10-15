9345371fa0bec78da90f0c64cde0832381a7fde6
Luis Vielva <LuisVielva@gmail.com>
2025-10-15 17:48:39 +0200

    110 230

--- submission/task230.py
+++ submission/task230.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec(("g[:]=zip(*eval(re.sub(r'0(?=, 0.{"+str(3*len(g)-2)+"}0, 5)','%d',str(g)))[::-1]);")*4%(1,3,4,2))or g
+p=lambda g:exec(("g[:]=zip(*eval(re.sub('0(?=, 0.{%%d}0, 5)'%%(3*len(g)-2),'%d',str(g)))[::-1]);")*4%(1,3,4,2))or g
