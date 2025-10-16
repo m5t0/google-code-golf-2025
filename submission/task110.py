79ecbe42e8727fb48a42ef814511d7f72bfeaaa4
KazApps <kazuki_shogi@outlook.jp>
2025-10-16 20:56:49 +0900

    110

--- submission/task110.py
+++ submission/task110.py
@@ -1,2 +1,2 @@
 f=lambda g:[0in r and min(g,key=lambda s:(0in s,sum(x&~y for x,y in zip(r,s))))or r for r in g]
-p=lambda g:g[0][1]and f(g)or[*f([*zip(*g)])]
+p=lambda g:g[0][1]and f(g)or f([*zip(*g)])
