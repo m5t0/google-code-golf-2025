e4b82e18085003c9676f4b5a7bd86a68dad5d9d3
Luis Vielva <LuisVielva@gmail.com>
2025-10-16 10:32:35 +0200

    119

--- submission/task119.py
+++ submission/task119.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec('g[:]=eval(re.sub("0(?=(?!.{40}2).{40}[^0].{40}[1-9])","3",str([*zip(*g)][::-1])));'*40)or g
+p=lambda g:exec('g[:]=eval(re.sub("0(?=.{40}[38].{40}[382])","3",str([*zip(*g)][::-1])));'*40)or g
