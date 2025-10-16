6a79945cce79b24486fb31c22e51cbec52dd78c4
sktkmozt <kazu22kun@icolud.com>
2025-10-16 21:57:54 +0900

    task132-87bytes(短縮)

--- code/task132.py
+++ code/task132.py
@@ -1,2 +1,2 @@
-f=lambda g,t=0:[[t|(t:=t^x)for x in v]for v in zip(*g)]
-p=lambda g:f(f([[max({*v}&{*w})for w in zip(*g)]for v in g]))
+f=lambda g,t=0:[[t|(t:=t^max({*v}&{*w}))for w in g]for v in zip(*g)]
+p=lambda g:f(f(g))
