e07ba908945ea7bc6eb2ffe51e26bfa81893a15a
KazApps <kazuki_shogi@outlook.jp>
2025-10-16 21:31:04 +0900

    110

--- code/task110.py
+++ code/task110.py
@@ -1,2 +1,1 @@
-f=lambda g:[0in r and min(g,key=lambda s:(0in s,sum(x&~y for x,y in zip(r,s))))or r for r in g]
-p=lambda g:g[0][1]and f(g)or f([*zip(*g)])
+p=lambda g:[[max(g,key=lambda V:sum(a==b for a,b in zip(v,V)if V[j]))[j]for j in range(29)]for v in g]
