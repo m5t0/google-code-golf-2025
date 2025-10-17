ebe114c90cacafbf128047f3e27c85c51c27a653
Luis Vielva <LuisVielva@gmail.com>
2025-10-17 18:28:10 +0200

    015

--- code/task015.py
+++ code/task015.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec('g[:]=zip(*eval(re.sub("0(?=.{31}2)","4",re.sub("0(?=.{28}1)","7",str(g))))[::-1]);'*4)or g
+p=lambda g:exec('w=re.sub;g[:]=zip(*eval(w("0(?=.{31}2)","4",w("0(?=.{28}1)","7",str(g))))[::-1]);'*4)or g
