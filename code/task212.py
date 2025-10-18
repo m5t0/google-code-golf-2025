8d829d47b101b125c9da7396457125fc59ac6af4
sktkmozt <kazu22kun@icolud.com>
2025-10-18 23:25:28 +0900

    task212-125bytes(短縮)

--- code/task212.py
+++ code/task212.py
@@ -1,2 +1,2 @@
-f=lambda g:[(t:=0)or[(t:=[[[t,2][x==2],5-4*(x==1)][t>4],5][x>4])%5or x for x in v[::-1]]for v in g]
+f=lambda g:[(t:=0)or[(t:=[[t,s:=2-(t==5)][x==s],5][x>4])%5or x for x in v[::-1]]for v in g]
 p=lambda g:[*zip(*f(f(zip(*g))))]
