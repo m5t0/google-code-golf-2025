1bbfef64dc7cbe8ef93d01ac8f5e468121862619
sktkmozt <kazu22kun@icolud.com>
2025-10-18 23:01:51 +0900

    task212-133bytes(短縮)

--- submission/task212.py
+++ submission/task212.py
@@ -1,1 +1,2 @@
-p=lambda g:[[w[i]or sum(({1,5}<{*(s:=w[i::k])}and(s.index(5)>s.index(1)))+(2in s and 5in w[i::-k])*2for k in[-1,1])for w in zip(*g)]for i in range(len(g))]
+f=lambda g:[(t:=0)or[(t:=[[[t,2][x==2],5-4*(x==1)][t>4],5][x>4])%5or x for x in v[::-1]]for v in g]
+p=lambda g:[*zip(*f(f(zip(*g))))]
