0089c3b16799ab3343dbe1302bbc2a5c2d73187b
moto <mo_to1015@yahoo.co.jp>
2025-10-19 02:46:03 +0900

    shorten task020 (172 bytes)

--- submission/task020.py
+++ submission/task020.py
@@ -1,4 +1,4 @@
 def p(e,r=range(10)):
  p,m=zip(*((p,m)for p in r for m in r if e[p][m]))
- for s,t in zip(p,m):exec("a,b=max(p)-t+min(m),min(m)+s-min(p);e[a][b]=e[s][t];s,t=a,b;"*4)
+ for s,t in zip(p,m):exec("a,b=max(p)-t+min(m),min(m)+s-min(p);e[s:=a][t:=b]=e[s][t];"*4)
  return e
