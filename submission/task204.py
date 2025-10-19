2dc27b291862505f5867dda018c78175230b4ceb
KazApps <kazuki_shogi@outlook.jp>
2025-10-19 10:47:18 +0900

    204

--- submission/task204.py
+++ submission/task204.py
@@ -1,2 +1,1 @@
-f=lambda v,I:1in{*v[:I]}&{*v[I:]}and v[:I][::-1].index(1)+v[I:].index(1)
-p=lambda g,e=enumerate:[[w[i]or(x:=f(v,j))==f(w,i)>0and 2+x%2*5for j,w in e(zip(*g))]for i,v in e(g)]
+p=lambda g:[(t:=0)or[(t:=[t==1and 2+v[j:].index(1)%2*5or(1<t<8)*t,1+(t>0)*8][x>0])%8for j,x in enumerate(v)]for v in g]
