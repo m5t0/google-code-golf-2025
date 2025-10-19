20ca4ed53a2f02c8cfe781fcbf463aece26749d1
Joonha Kwon <joonhakwon@JoonhanoMacBook-Pro.local>
2025-10-19 16:48:45 +0900

    Improve task002

--- submission/task002.py
+++ submission/task002.py
@@ -1,1 +1,1 @@
-p=lambda g,k=67:g*(k<0)or p([[(a+2^6,a or(b>3)*4)[k>0]for a,b in zip(x,(4,)+x)]for x in zip(*g[::-1])],k-1)
+p=lambda g,k=70:g*~k or p([[(a+2^6,a or(b>3)*4)[k>1]for a,b in zip(x,(4,)+x)]for x in zip(*g[::-1])],k-1)
