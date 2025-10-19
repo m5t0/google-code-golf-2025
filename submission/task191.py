f63445a75aedc5178106f77a3ca381763f439660
Luis Vielva <LuisVielva@gmail.com>
2025-10-19 18:06:26 +0200

    185

--- submission/task191.py
+++ submission/task191.py
@@ -1,3 +1,9 @@
-#coding:L1
-import zlib
-exec(zlib.decompress(bytes("uQÍj„0¾ûsMÔ-lÕñEBš´ã’(}ú®Z¶ë%0ßÏÌ÷‘~°pg×	DüŠŸÌ	mx³à{y•Q×µ,Ì›§~øfoŒÇpQª¬ä°¾yÑôH¸¤+nóˆáFn`!#€Ìà	vğº^Ü@€òC”—•Ø™é³SQXğ¤³gÇ1r“İ¦‰±²ê|n•¢Ní”Û”Ú²âmÑ¡Ó{Ş¨Toô5¦½™ÃkÏzvbi«Îéİaô¦l1â³–ó£üëñ×¼…—¹êçugÅ€µ&YHŸ’ğ©ÓKWš,ä^):>a4M„$BaXb p¿","L1"),-9))
+def p(x):
+ u=sum(x,[]);p=528-u[::-1].index(1);e=u.index(1);N=23;r=(p-e)//N+1;d=n=p%N-e%N+1;l=range(r*n);R=range(-9,24)
+ for o in range(8):
+  for i in R:
+   for a in R:
+    for u,f in[*enumerate(l)]*all((N>i+f//n>-1<a+f%n<N)<1>=x[e//N+u//d][e%N+u%d]or(N>i+f//n>-1<a+f%n<N and(x[e//N+u//d][e%N+u%d]<3>x[i+f//n][a+f%n]or x[e//N+u//d][e%N+u%d]==x[i+f//n][a+f%n]))for u,f in enumerate(l)):
+     if N>i+f//n>-1<a+f%n<N:x[i+f//n][a+f%n]=x[e//N+u//d][e%N+u%d]
+  l=[[n-1-i%n,i%n][o&3>2]*r+i//n for i in l];r,n=n,r
+ return x
