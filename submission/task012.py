96cf6f578d80a739f391e60367ee9b7849c8183c
KazApps <kazuki_shogi@outlook.jp>
2025-10-19 10:24:36 +0900

    012

--- submission/task012.py
+++ submission/task012.py
@@ -1,3 +1,3 @@
-#coding:L1
-import zlib
-exec(zlib.decompress(bytes('=Š1†¸§ ½ŞqƒN÷"”µUè&Ö÷_5‚ïûş¼J…¡£GÊ£sŞY6¨o‚E­şM5³3C¤4œÇTÑFáSY‘—çe÷´÷îˆ¡­yÍB«°Ä)ÕIulÊkiPùN@?',"L1"),-9))
+import re
+s=r"g[:]=zip(*eval(re.sub(r'0(?=(.%s)',r'\3',str(g)))[::-1]);"
+p=lambda g:exec(s%r")( )(.).{5}\3"*4+s%r"{34}|.{75})([^0]), \2, (.)"*4)or g
