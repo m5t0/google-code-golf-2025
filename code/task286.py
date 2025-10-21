1fffc4b4c20631998f22307c7ab9677affe33a3c
Luis Vielva <LuisVielva@gmail.com>
2025-10-21 09:54:34 +0200

    286

--- code/task286.py
+++ code/task286.py
@@ -1,8 +1,1 @@
-def p(a,e=enumerate):
- f=sum(a,[]);p=sum({*f})-8
- for o in f:
-  for o,m in e(a):
-   for n,f in e(m):
-    for n,u in(o-1,n),(o+1,n),(o,n-1),(o,n+1):
-     if a[n:n+1]and a[n][u:u+1]==[0]and f&7:a[n][u]=p-f
- return a
+def p(g,r=str.replace):n,m=map(str,{*sum(g,[])}-{0,8});exec("g[:]=zip(*eval(r(r(str(g),n+', 0',n+','+m),m+', 0',m+','+n))[::-1]);"*192);return g
