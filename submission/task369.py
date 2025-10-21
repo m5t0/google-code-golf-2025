555c9bd472f8fb19d98937ab1308d091a75aeb83
Joonha Kwon <joonhakwon@JoonhanoMacBook-Pro.local>
2025-10-21 21:48:43 +0900

    Improve task369

--- submission/task369.py
+++ submission/task369.py
@@ -1,2 +1,2 @@
-d=0,1,0,-1
-p=lambda g,r=range(10):[[g[i][j]or 4-(f:=lambda y,x,p:1+sum(f(I,J,(y,x))for k in range(4)if-1<(I:=y+d[k])<10>(J:=x+d[~k])>=0and g[I][J]<1!=(I,J)!=p))(i,j,(i,j))for j in r]for i in r]
+s="g[:]=zip(*eval(str(g).replace('%s'))[::-1]);"
+p=lambda g:exec(s%"0','3"*4+s%"3, 3','2,2"*4+s%"2, 3','1,1"*4+s%"1, 2','1,1"*4)or g
