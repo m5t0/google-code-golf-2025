2b3ab8d14f33c92ca87dc3d580247da9ff5b66a0
KazApps <kazuki_shogi@outlook.jp>
2025-10-22 09:56:41 +0900

    094

--- submission/task094.py
+++ submission/task094.py
@@ -1,2 +1,2 @@
-f=lambda l,i:l[i].count(1)>0<l[i-2].count(1)>4<l[i+2].count(1)
-p=lambda g,e=enumerate:[[w%2or[8,6][f(g,i)|f([*zip(*g)],j)]for j,w in e(v)]for i,v in e(g)]
+f=lambda l,i:(1in l[i])*l[i-2].count(1)>4<l[i+2].count(1)
+p=lambda g,e=enumerate:[[x-x//4*(f(g,i)|f([*zip(*g)],j))for j,x in e(v)]for i,v in e(g)]
