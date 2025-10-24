39133ea275283b102efba29210902b92c077bf8b
KazApps <kazuki_shogi@outlook.jp>
2025-10-24 23:41:42 +0900

    138

--- code/task138.py
+++ code/task138.py
@@ -1,1 +1,3 @@
-f=lambda g,s=0:[v for v in zip(*g)if s|(s:=s^all(v))];h=lambda g:[[max({w}|{v[0]}&{*v[j:]}|{v[-1]}&{*v[:j]})for j,w in enumerate(v)]for v in zip(*g)];p=lambda g:h(h(f(f(g))))
+f=lambda g,s=0:[v for v in zip(*g)if s|(s:=s^all(v))]
+h=lambda g:[[max({w}|{v[0]}&{*v[j:]}|{v[-1]}&{*v[:j]})for j,w in enumerate(v)]for v in zip(*g)]
+p=lambda g:h(h(f(f(g))))
