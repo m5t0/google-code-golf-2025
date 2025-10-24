37649cbd413a92b7767971d797c56942b0903abd
KazApps <kazuki_shogi@outlook.jp>
2025-10-24 23:09:12 +0900

    064

--- code/task064.py
+++ code/task064.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:[g,exec("g[:]=zip(*eval(re.sub('{1}, {0}(?=[^])]* [^{0}{1}])','{1},{1}',str(g)))[::-1]);".format(*re.findall(r'(.), ((?!\1).), \1',str(g+[*zip(*g)]))[0])*56)][0]
+p=lambda g:exec("g[:]=zip(*eval(re.sub('{1}, {0}(?=[^])]* [^{0}{1}])','{1},{1}',str(g)))[::-1]);".format(*re.findall(r'(.), ((?!\1).), \1',str(g+[*zip(*g)]))[0])*56)or g
