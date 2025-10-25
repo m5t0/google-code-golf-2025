5d77ec1588498c0e76c31020990d19aac4ea316d
KazApps <kazuki_shogi@outlook.jp>
2025-10-26 00:52:16 +0900

    merge

--- submission/task131.py
+++ submission/task131.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec(r"g[:]=[*zip(*eval(re.sub('2\)(.*), \([^3)]*\)((, \(.*?3.*?\))+)',r'2)\2,[8]*len(g[0])\1',str(g)))[::-1])];"*8)or g
+p=lambda g,k=7:-k*g or p(eval(re.sub('2\)(.*), \([^3)]*\)((, \(.*?3.*?\))+)',r'2)\2,[8]*len(g)\1',str([*zip(*g)])))[::-1],k-1)
