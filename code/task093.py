a50f761485020699c8dccbdc5dffcfd4e71b355a
KazApps <kazuki_shogi@outlook.jp>
2025-10-26 18:13:32 +0900

    093

--- code/task093.py
+++ code/task093.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec(r"g[:]=zip(*eval(re.sub('[^05],([0, ]*)5',r'\1 5,5',str(g[::-1]))));"*12)or g
+p=lambda g:exec(r"g[:]=zip(*eval(re.sub('[^05],([0, ]*5)',r'\1,5',str(g[::-1]))));"*12)or g
