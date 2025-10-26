cd54f60775e46dabe5b8f60da4fb5ba704d31604
Joonha Kwon <joonhakwon@JoonhanoMacBook-Pro.local>
2025-10-26 11:19:58 +0900

    Improve task260

--- code/task260.py
+++ code/task260.py
@@ -1,3 +1,3 @@
-f=enumerate
-l=lambda n,m:m([d-a for d,z in f(n)for a,i in f(zip(*n))if(z[a-1]==5,i[d-1]==5)[m(0,1)]>i[d]]+[15-30*m(0,1)])
-p=lambda n:[[i-5and(i or(d-a)in(l(n,max)+1,l(n,min)-1)and sum({*sum(n,[])})-5)for a,i in f(z)]for d,z in f(n)]
+import re
+s="g[:]=eval(re.sub('%s',str(g)));"+"g[:]=zip(*g[::-1]);"*2
+p=lambda g:exec(s%r"0, 0(?=.{28}5, 0.* ([^05]),)',r'0,\1"*5+s%r"0(?=.{34}([^ ]))',r'\1"*12+s%"5','0")or g
