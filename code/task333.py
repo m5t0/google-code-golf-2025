256e4178e5613615af6f510cd4c84b2276afe85b
KazApps <kazuki_shogi@outlook.jp>
2025-10-26 18:11:11 +0900

    333

--- code/task333.py
+++ code/task333.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec(r"g[:]=zip(*eval(re.sub('3,([0, ]*)0, (.)',r'3,\1\2,\2',str(g)))[::-1]);"*24)or g
+p=lambda g:exec(r"g[:]=zip(*eval(re.sub('(3,[0, ]*)0, (.)',r'\1\2,\2',str(g)))[::-1]);"*24)or g
