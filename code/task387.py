1485c8d333a08e85d6765f3500a764cf48b5702c
KazApps <kazuki_shogi@outlook.jp>
2025-10-27 11:13:10 +0900

    387

--- code/task387.py
+++ code/task387.py
@@ -1,13 +1,11 @@
-def p(m,n=range):
- d=0;r=len(m)
- for f in n(r):
-  if len(e:=[f for f,l in enumerate(m[f])if l%5])==2:
-   p,e=e
-   for l in n(9):
-    if l-4:m[f+l//3-1][p+l%3-1]=m[f][e];m[f+l//3-1][e+l%3-1]=m[f][p]
-   for l in n(1,e-p):
-    if min(l,e-p-l)%2==0:m[f][p+l]=5
-   if d<1:
-    for l in n(1,d:=(o:=[f for f in n(r)if m[f][p]%5])[3]-o[1]):
-     if min(l,d-l)%2==0:m[o[1]+l][p]=m[o[1]+l][e]=5
- return m
+def p(r):
+ (a,t),*s,(u,e)=m=[(u,m)for u,e in enumerate(r)for m,i in enumerate(e)if i]
+ for f,(u,m) in enumerate(m):
+  for n in-1,0,1:
+   for i in-1,0,1:
+    if n|i:r[u+n][m+i]=r[a][[e,t][0<f<3]]
+ for m in range(2,(u-a)//2+1,2):
+  for n in t,e:r[a+m][n]=r[u-m][n]=5
+ for m in range(2,(e-t)//2+1,2):
+  for n in a,u:r[n][t+m]=r[n][e-m]=5
+ return r
