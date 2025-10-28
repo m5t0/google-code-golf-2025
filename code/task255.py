ab6a0e2e82b000a0671832a8066922c8a7c09854
Luis Vielva <LuisVielva@gmail.com>
2025-10-28 20:32:52 +0100

    255

--- code/task255.py
+++ code/task255.py
@@ -1,12 +1,3 @@
-def p(g,r=range):
- g=[[0]*32,*[[0,*v,0]for v in g],[0]*32];a=eval(str(g))
- for _ in r(8):
-  i=-1;g=[*zip(*g)][::-1];a=[*map(list,zip(*a))][::-1]
-  while i<33:
-   i+=1;I=i
-   for j in r(33,i,-1):
-    for k in r(20):
-     for V in a[i+1:j-1]*(sum(sum(v[:-k-1])for v in g[i:j])<1):
-      V[:-k-2]=[3]*(30-k);i=j
-      for K in 3,4:V[-k-K]-=3*(a[I][-k-K]<a[I][27-k])
- return[v[1:-1]for v in a[1:-1]]
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes("EOMjÅ Þç.Õ§c¡ÅÄ“.„j*øÒh9}ý¡¯‚0óý2ŸÎ£{¶êÃn«#j@^Œ†Ê‰Ñ20z³ÑøïÝ(lÈÖI3í~lÄg:°'d@/ÍŠ?jÊš‹¹¤Ñ;ì˜zb@).ªèÓî8†3±ÆòGÛõ¢Cy‘²† üÐbv:×¹V¤^!%ËŒ‹VÔ‰Ø‰iìXmä‡P©ÄS|ægû7(BþOƒ¬’!KÏ¬Ï6Íd4Èâ”#dÎ:5¶Ç_Õ(Ù›²P”—áZRÀ™¾.mœÞy4d@‡KùØàQ/}õ†¾›_","L1"),-9))
