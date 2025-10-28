d83710739a3285d30222033e912d5b953c169180
Luis Vielva <LuisVielva@gmail.com>
2025-10-29 00:50:29 +0100

    191

--- submission/task191.py
+++ submission/task191.py
@@ -1,9 +1,3 @@
-def p(x):
- u=sum(x,[]);p=528-u[::-1].index(1);e=u.index(1);N=23;r=(p-e)//N+1;d=n=p%N-e%N+1;l=range(r*n);R=range(-9,24)
- for o in range(8):
-  for i in R:
-   for a in R:
-    for u,f in[*enumerate(l)]*all((N>i+f//n>-1<a+f%n<N)<1>=x[e//N+u//d][e%N+u%d]or(N>i+f//n>-1<a+f%n<N and(x[e//N+u//d][e%N+u%d]<3>x[i+f//n][a+f%n]or x[e//N+u//d][e%N+u%d]==x[i+f//n][a+f%n]))for u,f in enumerate(l)):
-     if N>i+f//n>-1<a+f%n<N:x[i+f//n][a+f%n]=x[e//N+u//d][e%N+u%d]
-  l=[[n-1-i%n,i%n][o&3>2]*r+i//n for i in l];r,n=n,r
- return x
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes("uQßj… ¾?Oá ¥kƒê×‹ˆB:§‘¢§ŸÕ 8«›ÀïŸß—½±h$Öd!ÎßÄ1©hãá£üäVÖ5êmğ½Yˆ Íö<hğEQV|Ü¾¹hğxğ~^`ÒşËšÒÃ„f4xt€O.¶;w80²äİ©ƒ[¯¹ƒ´Ì'Zfë0’ó.¶P•iç)«.æ©dè¸h×ÜãĞ–mEN­mQ%÷Â¦kö=¹±´UçäáPr×§t#xÕRzAÿvümEƒE—½ê×¸»a)f)<âÀb’Ê¿weúSyLçC,ªÑ,@`ú&ó3O¹_","L1"),-9))
