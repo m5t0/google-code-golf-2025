67918c5af73e71306a046c14d5773972f734c8ec
Luis Vielva <LuisVielva@gmail.com>
2025-10-29 16:21:56 +0100

    363

--- code/task363.py
+++ code/task363.py
@@ -1,8 +1,3 @@
-def p(g,r=range(10)):
- I=sum(g,[]).index(2);a=[(i-I//10,j-I%10)for i in r for j in r if g[i][j]&2]
- for I,J in(g[0][5:7]==[0,2])*a:g[1+I][8+J]=2
- for j in r:
-  for i in r:
-   for I,J in a*all(i+x<10>j+y>=g[i+x][j+y]<1for x,y in a):g[i+I][j+J]=2
- if g[5].count(2)>7:g[1][3:7]=[0]*4
- return g
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes("eÁnƒ0†ï<…/› m’­êÄ^Äò¡ÂfBÊÜ‰5Ò„×Ï¿>,#ü¶]ó·áÉ'eù3Á~ôtò®M}ñÎ÷&…ù¦_b¥g4À(„‰^UëBÛ¼l£#<wŠ]ÈÖCÇè%üh2ÅPí}¥o¥²£5õ-%#^½ëS“ûÈ(EœñÕo—¶ÛœÊÏ5øLÇáõa‚í/¥Šğ­”.Éõ{³<ò¬Àÿ","L1"),-9))
