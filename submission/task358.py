39ba96b99be704e37f68e00ee8ac588d5b64cf9f
sktkmozt <kazu22kun@icolud.com>
2025-10-30 08:51:32 +0900

    task358-113bytes(短縮)

--- submission/task358.py
+++ submission/task358.py
@@ -1,2 +1,1 @@
-f=lambda v,j:(s:=len({*v})-1)>1and sum(v[j%s::s])
-p=lambda g,e=enumerate:[[f(v,j)or f(w,i)for j,w in e(zip(*g))]for i,v in e(g)]
+f=lambda g:[[w or(s:=len({*v})-1)>1and sum(v[j%s::s])for j,w in enumerate(v)]for v in zip(*g)];p=lambda g:f(f(g))
