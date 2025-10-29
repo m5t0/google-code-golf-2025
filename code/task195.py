82d4aee8abec6457363306b5e894d1ce6f8a69c5
sktkmozt <kazu22kun@icolud.com>
2025-10-30 03:21:48 +0900

    task195-111bytes(短縮)

--- code/task195.py
+++ code/task195.py
@@ -1,1 +1,1 @@
-def p(g):h=(f:=lambda g:[*filter(any,zip(*g))])(f(g[::3])[::3]);return[[c&d for c in a for d in b]for a in h for b in h]
+f=lambda g:[*filter(any,zip(*g[::3]))];p=lambda g:[[c&d for c in a for d in b]for a in f(f(g))for b in f(f(g))]
