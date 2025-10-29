6669339a152c0067411e642bd1475731dbdd5dee
Luis Vielva <LuisVielva@gmail.com>
2025-10-29 13:51:47 +0100

    324

--- code/task324.py
+++ code/task324.py
@@ -1,3 +1,3 @@
 #coding:L1
 import zlib
-exec(zlib.decompress(bytes("eNGVƒ!Þÿ§`å›‰Û./ÂKÏNÇîÝìº¡|}ÍZ8Ð8ìÄ“³áË˜O IMp´—Ñ†Äkxîñ+øQòÅÊf“pd)Ò‚–r?:‘‘G6 ‘Ú¥®&Ø‰5g!vF¡m¡>5ÞVxùÖ°%-:±—êŸk2òÿÑ§ƒžÆI'‚T‰±R¹Qpj~ÿ,s´ÊÅ“y.Û_'ï§†|PÍNÜj*»/ð™/‹EF²¥ŠSF)([© ¨nzó‹e„Ü? ”õÅ}Ûúd[žÃwØä×²É¯Ñ“7","L1"),-9))
+exec(zlib.decompress(bytes("eOGVÄ0Ýç^ñ¤ACÛM1ñ3©Š©J§swlÓñ&E¿7Üªn3%zZîÀ‘±¸›ôÔ37ð²’7ºá'-Gu·ÈŒ»œJ¨ÑÓ®§Eß²x5Å—9±˜©«VŸA]‰UÛjŸÏÃ¹IÎÛ`ÖÐ©I›Dewuz}¾êaåÐfjÔ˜Ä‘UŠcèãA~úZÎ°5b¯äÅox³±jy!ü8Ã'mèC‡øóAíÜz‰.)÷‘</£¾3ù+š³U)OPVðz@­Ã—¬óØÁS¢Â™MÚÙd€}","L1"),-9))
