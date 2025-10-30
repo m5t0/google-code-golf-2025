2b7d0433a107625b33fb1b58b609fc1710f5ab14
sktkmozt <kazu22kun@icolud.com>
2025-10-30 12:40:40 +0900

    task251-103bytes(短縮)

--- code/task251.py
+++ code/task251.py
@@ -1,5 +1,1 @@
-def p(g):
- def f(i,j):
-  for k in[-1,1]*(-n<i<n>j>-n<1>g[i][j]):g[i][j]=1;f(i+k,j);f(i,j+k)
- for i in range(n:=len(g)):f(i,0);f(0,i);f(i,-1);f(-1,i)
- return[[[1,0,2][x]for x in v]for v in g]
+p=lambda g,k=7:-k*g or p([(b:=4)and[b:=[(a*2+1)%3,a or-3&b][k>0]for a in v]for v in zip(*g[::-1])],k-1)
