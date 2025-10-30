08e187220520c0d1650c056c3b5cbd96eab9f408
sktkmozt <kazu22kun@icolud.com>
2025-10-30 12:10:48 +0900

    task281-155bytes(短縮)

--- submission/task281.py
+++ submission/task281.py
@@ -1,3 +1,1 @@
-e=enumerate
-f=lambda g,i:((I:=[i for i,v in e(g)if sum(v)])[0]-i)*(i-I[-1])
-p=lambda g:[[sorted({*(V:=sum(g,[]))}-{8},key=V.count)[f(g,i)*(t:=f(zip(*g),j))<1]*(f(g,i)>-1<t)for j,w in e(v)]for i,v in e(g)]
+p=lambda g,k=3,c=0,d=1:-k*g or p([*zip(*[[c:=c+(len({*v})==2),d:=d-(8in v),[v,*filter(any,g)][c>1and('8'in str(g))*2*d+(8in v)]][2]for v in g])][::-1],k-1)
