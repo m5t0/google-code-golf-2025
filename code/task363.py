b699c9aa4594f49bbdef0c757075bcc819b10aed
KazApps <kazuki_shogi@outlook.jp>
2025-10-30 12:04:30 +0900

    merge

--- code/task363.py
+++ code/task363.py
@@ -1,8 +1,8 @@
-def p(n,q=range(10)):
- f=sum(n,[]).index(2);r=[(e-f//10,u-f%10)for e in q for u in q if n[e][u]&2]
- for f,i in(n[0][5:7]==[0,2])*r:n[1+f][8+i]=2
- for u in q:
-  for e in q:
-   for f,i in r*all(e+f<10>u+q>=n[e+f][u+q]<1for f,q in r):n[e+f][u+i]=2
- if n[5].count(2)>7:n[1][3:7]=[0]*4
- return n
+def p(f):
+ c=sum(f,[]).index(2);_=[(d-c//10,e-c%10)for d in range(10) for e in range(10) if f[d][e]&2]
+ for c,o in(f[0][5:7]==[0,2])*_:f[1+c][8+o]=2
+ for e in range(10):
+  for d in range(10):
+   for c,o in _*all(d+c<10>e+o>=f[d+c][e+o]<1for c,o in _):f[d+c][e+o]=2
+ if f[5].count(2)>7:f[1][3:7]=[0]*4
+ return f
