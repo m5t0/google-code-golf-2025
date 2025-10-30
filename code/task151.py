e8e6d38fec14a91168e197e2897fc445b3035099
KazApps <kazuki_shogi@outlook.jp>
2025-10-30 15:47:05 +0900

    151

--- code/task151.py
+++ code/task151.py
@@ -1,2 +1,1 @@
-f=lambda g:g.index(max(g))-1
-def p(g):j=f(g[0]);g[i+2]=a=g[i:=f(g)];a[j:j+3]=4,4,4;g[i+1][j:j+3:2]=4,4;return g
+def p(g):j=g[0].index(max(g[0]))-1;k=g.index(max(g));g[k+1]=a=g[k-1];a[j:j+3]=4,4,4;g[k][j:j+3:2]=4,4;return g
