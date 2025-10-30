7df21c80f8ef371cba2076114b5e84d5606506d0
KazApps <kazuki_shogi@outlook.jp>
2025-10-30 11:30:11 +0900

    042

--- submission/task042.py
+++ submission/task042.py
@@ -1,1 +1,1 @@
-p=lambda g,r=range(10):[[g[i][j]|any(0<j+(y:=2*(x:=max(map(sum,zip(*g)))//3*(k-k%2-1))*(t:=k%2*2-1))<10>i+y*t>0and g[i+x][j+y]==g[i+y*t][j+x*t]==3for k in r[:4])*8for j in r]for i in r]
+p=lambda g,r=range(10):[[g[i][j]|any(0<j+(x:=max(map(sum,zip(*g)))//3*a)*b*2<9>i+x*2>0<g[i+x][j+x*b*2]==g[i+x*2][j+x*b]for a in(-1,1)for b in(-1,1))*8for j in r]for i in r]
